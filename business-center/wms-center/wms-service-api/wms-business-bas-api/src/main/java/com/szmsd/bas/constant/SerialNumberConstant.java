5c2a4bfe95d218e7d78fe3c4b38fc8039ab042e5
zhangyuyuan <zhangyuyuan@szmsd.com>
2021-03-05 17:33:58 +0800

    1.修改配置信息。

--- business-center/wms-center/wms-service-api/wms-business-bas-api/src/main/java/com/szmsd/bas/constant/SerialNumberConstant.java
+++ business-center/wms-center/wms-service-api/wms-business-bas-api/src/main/java/com/szmsd/bas/constant/SerialNumberConstant.java
@@ -5,4 +5,9 @@ package com.szmsd.bas.constant;
  * @date 2021-03-04 14:39
  */
 public final class SerialNumberConstant {
+
+    /**
+     * 出库单号
+     */
+    public static final String DEL_OUTBOUND_NO = "DEL_OUTBOUND_NO";
 }
