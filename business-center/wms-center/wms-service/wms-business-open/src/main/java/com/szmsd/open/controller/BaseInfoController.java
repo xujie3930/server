41a64c18a691c3dc1e710a36953a8783f4f13316
梁超 <1572578128@qq.com>
2021-03-09 14:59:06 +0800

    open返还参数修改

--- business-center/wms-center/wms-service/wms-business-open/src/main/java/com/szmsd/open/controller/BaseInfoController.java
+++ business-center/wms-center/wms-service/wms-business-open/src/main/java/com/szmsd/open/controller/BaseInfoController.java
@@ -2,8 +2,7 @@ package com.szmsd.open.controller;
 
 import com.szmsd.bas.api.service.BasWarehouseClientService;
 import com.szmsd.bas.dto.AddWarehouseRequest;
-import com.szmsd.common.core.domain.R;
-import com.szmsd.common.core.web.controller.BaseController;
+import com.szmsd.open.vo.ResponseVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.validation.annotation.Validated;
@@ -24,8 +23,9 @@ public class BaseInfoController extends BaseController {
 
     @PostMapping("/warehouse")
     @ApiOperation(value = "#A1 创建/更新仓库")
-    public R add(@RequestBody @Validated AddWarehouseRequest addWarehouseRequest) {
-        return basWarehouseClientService.saveOrUpdate(addWarehouseRequest);
+    public ResponseVO add(@RequestBody @Validated AddWarehouseRequest addWarehouseRequest) {
+        basWarehouseClientService.saveOrUpdate(addWarehouseRequest);
+        return ResponseVO.ok();
     }
 
 }
