0d73dc61856f903318ad2b289473d9a76393f52c
zhangyuyuan <zhangyuyuan@szmsd.com>
2021-03-09 14:46:56 +0800

    1.修改open返回参数。

--- business-center/wms-center/wms-service/wms-business-open/src/main/java/com/szmsd/open/controller/DelOutboundController.java
+++ business-center/wms-center/wms-service/wms-business-open/src/main/java/com/szmsd/open/controller/DelOutboundController.java
@@ -1,7 +1,5 @@
 package com.szmsd.open.controller;
 
-import com.szmsd.common.core.domain.R;
-import com.szmsd.common.core.web.controller.BaseController;
 import com.szmsd.common.log.annotation.Log;
 import com.szmsd.common.log.enums.BusinessType;
 import com.szmsd.delivery.api.service.DelOutboundClientService;
@@ -9,6 +7,7 @@ import com.szmsd.delivery.dto.PackageMeasureRequestDto;
 import com.szmsd.delivery.dto.ShipmentContainersRequestDto;
 import com.szmsd.delivery.dto.ShipmentPackingMaterialRequestDto;
 import com.szmsd.delivery.dto.ShipmentRequestDto;
+import com.szmsd.open.vo.ResponseVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
@@ -39,32 +38,36 @@ public class DelOutboundController extends BaseController {
     @PostMapping("/shipment")
     @ApiOperation(value = "出库管理 - #D1 接收出库单状态", position = 100)
     @ApiImplicitParam(name = "dto", value = "ShipmentRequestDto", dataType = "ShipmentRequestDto")
-    public R<Integer> shipment(@RequestBody @Validated ShipmentRequestDto dto) {
-        return R.ok(delOutboundClientService.shipment(dto));
+    public ResponseVO shipment(@RequestBody @Validated ShipmentRequestDto dto) {
+        delOutboundClientService.shipment(dto);
+        return ResponseVO.ok();
     }
 
     @Log(title = "出库单模块", businessType = BusinessType.UPDATE)
     @PostMapping("/shipment/measure")
     @ApiOperation(value = "出库管理 - #D2 接收出库包裹测量信息", position = 200)
     @ApiImplicitParam(name = "dto", value = "PackageMeasureRequestDto", dataType = "PackageMeasureRequestDto")
-    public R<Integer> shipmentMeasure(@RequestBody @Validated PackageMeasureRequestDto dto) {
-        return R.ok(delOutboundClientService.shipmentMeasure(dto));
+    public ResponseVO shipmentMeasure(@RequestBody @Validated PackageMeasureRequestDto dto) {
+        delOutboundClientService.shipmentMeasure(dto);
+        return ResponseVO.ok();
     }
 
     @Log(title = "出库单模块", businessType = BusinessType.UPDATE)
     @PostMapping("/shipment/packing")
     @ApiOperation(value = "出库管理 - #D3 接收出库包裹使用包材", position = 300)
     @ApiImplicitParam(name = "dto", value = "ShipmentPackingMaterialRequestDto", dataType = "ShipmentPackingMaterialRequestDto")
-    public R<Integer> shipmentPacking(@RequestBody @Validated ShipmentPackingMaterialRequestDto dto) {
-        return R.ok(delOutboundClientService.shipmentPacking(dto));
+    public ResponseVO shipmentPacking(@RequestBody @Validated ShipmentPackingMaterialRequestDto dto) {
+        delOutboundClientService.shipmentPacking(dto);
+        return ResponseVO.ok();
     }
 
     @Log(title = "出库单模块", businessType = BusinessType.UPDATE)
     @PostMapping("/shipment/containers")
     @ApiOperation(value = "出库管理 - #D4 接收批量出库单类型装箱信息", position = 400)
     @ApiImplicitParam(name = "dto", value = "ShipmentContainersRequestDto", dataType = "ShipmentContainersRequestDto")
-    public R<Integer> shipmentContainers(@RequestBody @Validated ShipmentContainersRequestDto dto) {
-        return R.ok(delOutboundClientService.shipmentContainers(dto));
+    public ResponseVO shipmentContainers(@RequestBody @Validated ShipmentContainersRequestDto dto) {
+        delOutboundClientService.shipmentContainers(dto);
+        return ResponseVO.ok();
     }
 
 }
