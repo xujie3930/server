6d4a360407a3e67570d893e5859aae18af13ebb9
lihao <lihao@szmsd.com>
2021-03-15 17:01:15 +0800

    仓租管理添加、修改、查询

---
+++ business-center/wms-center/wms-service-api/wms-business-chargerules-api/src/main/java/com/msd/chargerules/dto/WarehouseOperationDTO.java
@@ -0,0 +1,34 @@
+package com.msd.chargerules.dto;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+@ApiModel(value = "WarehouseOperationDTO", description = "WarehouseOperationDTO对象")
+public class WarehouseOperationDTO implements Serializable {
+
+    @ApiModelProperty(value = "ID")
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    @ApiModelProperty(value = "计费天数（天）")
+    private Integer chargeDays;
+
+    @ApiModelProperty(value = "价格")
+    private BigDecimal price;
+
+    @ApiModelProperty(value = "计费单位")
+    private String chargeUnit;
+
+    @ApiModelProperty(value = "备注")
+    private String remark;
+
+
+
+}
