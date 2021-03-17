5de8d89ba23d0affe6be34ea81023c33036d306f
lihao <lihao@szmsd.com>
2021-03-17 17:20:36 +0800

    添加事务

--- business-center/wms-center/wms-service/wms-business-chargerules/src/main/java/com/szmsd/chargerules/service/impl/SpecialOperationServiceImpl.java
+++ business-center/wms-center/wms-service/wms-business-chargerules/src/main/java/com/szmsd/chargerules/service/impl/SpecialOperationServiceImpl.java
@@ -15,6 +15,7 @@ import com.szmsd.http.dto.SpecialOperationRequest;
 import com.szmsd.http.vo.ResponseVO;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -28,20 +29,21 @@ public class SpecialOperationServiceImpl extends ServiceImpl<SpecialOperationMap
     @Resource
     private HtpBasFeignService htpBasFeignService;
 
+    @Transactional
     @Override
     public int save(SpecialOperationDTO dto) {
         SpecialOperation domain = new SpecialOperation();
-        BeanUtils.copyProperties(dto,domain);
+        BeanUtils.copyProperties(dto, domain);
         int result = specialOperationMapper.insert(domain);
-        if(result > 0) {
+        if (result > 0) {
             //调用WMS接口
             SpecialOperationRequest specialOperationRequest = new SpecialOperationRequest();
             specialOperationRequest.setOperationType(domain.getOperationType());
             specialOperationRequest.setOperationTypeDesc(String.valueOf(domain.getId()));
             specialOperationRequest.setUnit(domain.getUnit());
             R<ResponseVO> response = htpBasFeignService.specialOperationType(specialOperationRequest);
-            if(response.getCode() != 200){
-                throw new BaseException("传wms失败"+response.getMsg());
+            if (response.getCode() != 200) {
+                throw new BaseException("传wms失败" + response.getMsg());
             }
         }
         return result;
@@ -55,8 +57,8 @@ public class SpecialOperationServiceImpl extends ServiceImpl<SpecialOperationMap
     @Override
     public List<SpecialOperation> listPage(SpecialOperationDTO dto) {
         QueryWrapper<SpecialOperation> where = new QueryWrapper<>();
-        if(StringUtils.isNotEmpty(dto.getOperationType())) {
-            where.eq("operation_type",dto.getOperationType());
+        if (StringUtils.isNotEmpty(dto.getOperationType())) {
+            where.eq("operation_type", dto.getOperationType());
         }
         return specialOperationMapper.selectList(where);
     }
@@ -64,8 +66,8 @@ public class SpecialOperationServiceImpl extends ServiceImpl<SpecialOperationMap
     @Override
     public SpecialOperation selectOne(BasSpecialOperation basSpecialOperation) {
         QueryWrapper<SpecialOperation> where = new QueryWrapper<>();
-        if(StringUtils.isNotEmpty(basSpecialOperation.getOperationType())) {
-            where.eq("operation_type",basSpecialOperation.getOperationType());
+        if (StringUtils.isNotEmpty(basSpecialOperation.getOperationType())) {
+            where.eq("operation_type", basSpecialOperation.getOperationType());
         }
         return specialOperationMapper.selectOne(where);
     }
