d68256d801fd3516e802315d0a4c9b5f6fa61ce4
lihao <lihao@szmsd.com>
2021-03-17 17:26:53 +0800

    添加注释

--- business-center/wms-center/wms-service-api/wms-business-http-api/src/main/java/com/szmsd/http/dto/SpecialOperationResultRequest.java
+++ business-center/wms-center/wms-service-api/wms-business-http-api/src/main/java/com/szmsd/http/dto/SpecialOperationResultRequest.java
@@ -1,5 +1,6 @@
 package com.szmsd.http.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
@@ -9,10 +10,13 @@ import java.io.Serializable;
 @Accessors(chain = true)
 public class SpecialOperationResultRequest implements Serializable {
 
+    @ApiModelProperty(value = "操作单号")
     private String operationOrderNo;
 
+    @ApiModelProperty(value = "状态（审核结果）通过：Pass 驳回：Reject")
     private String status;
 
+    @ApiModelProperty(value = "备注")
     private String remark;
 
 }
