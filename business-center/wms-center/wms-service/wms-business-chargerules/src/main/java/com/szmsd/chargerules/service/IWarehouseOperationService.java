e65627506fed93a7aa753dde36c82245ffb2ed0b
lihao <lihao@szmsd.com>
2021-03-18 14:00:20 +0800

    储存仓租计费

--- business-center/wms-center/wms-service/wms-business-chargerules/src/main/java/com/szmsd/chargerules/service/IWarehouseOperationService.java
+++ business-center/wms-center/wms-service/wms-business-chargerules/src/main/java/com/szmsd/chargerules/service/IWarehouseOperationService.java
@@ -4,6 +4,7 @@ package com.szmsd.chargerules.service;
 import com.msd.chargerules.domain.WarehouseOperation;
 import com.msd.chargerules.dto.WarehouseOperationDTO;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface IWarehouseOperationService {
@@ -13,5 +14,15 @@ public interface IWarehouseOperationService {
     int update(WarehouseOperation dto);
 
     List<WarehouseOperation> listPage(WarehouseOperationDTO dto);
+
+    /**
+     * 仓储服务计费
+     * @param days 商品存放天数
+     * @param cbm 商品体积
+     * @param warehouseCode 仓库
+     * @param dto dto
+     * @return 价格
+     */
+    BigDecimal charge(int days, BigDecimal cbm, String warehouseCode, List<WarehouseOperation> dto);
 
 }
