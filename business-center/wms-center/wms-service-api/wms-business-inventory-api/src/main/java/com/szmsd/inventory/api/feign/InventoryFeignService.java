fd984c1518111dbe1e39e0eeeb760ad366cd0d9d
梁超 <1572578128@qq.com>
2021-03-20 10:53:47 +0800

    获取库存SKU体积接口修改

--- business-center/wms-center/wms-service-api/wms-business-inventory-api/src/main/java/com/szmsd/inventory/api/feign/InventoryFeignService.java
+++ business-center/wms-center/wms-service-api/wms-business-inventory-api/src/main/java/com/szmsd/inventory/api/feign/InventoryFeignService.java
@@ -4,13 +4,21 @@ import com.szmsd.common.core.domain.R;
 import com.szmsd.inventory.api.BusinessInventoryInterface;
 import com.szmsd.inventory.api.factory.InventoryFeignFallback;
 import com.szmsd.inventory.domain.dto.InboundInventoryDTO;
+import com.szmsd.inventory.domain.dto.InventorySkuVolumeQueryDTO;
+import com.szmsd.inventory.domain.vo.InventorySkuVolumeVO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @FeignClient(contextId = "FeignClient.InventoryFeignService", name = BusinessInventoryInterface.SERVICE_NAME, fallbackFactory = InventoryFeignFallback.class)
 public interface InventoryFeignService {
 
     @PostMapping("/inventory/inbound")
     R inbound(@RequestBody InboundInventoryDTO receivingRequest);
+
+    @GetMapping("/inventory/skuVolume")
+    R<List<InventorySkuVolumeVO>> querySkuVolume(InventorySkuVolumeQueryDTO inventorySkuVolumeQueryDTO);
 }
