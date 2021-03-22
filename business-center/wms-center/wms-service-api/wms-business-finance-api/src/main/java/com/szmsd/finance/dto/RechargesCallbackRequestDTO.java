9fc697a42e855598328499321d7629537fa4609b
liulei <18296154928@163.com>
2021-03-22 19:15:29 +0800

    第三方充值回调接口参数body修改

--- business-center/wms-center/wms-service-api/wms-business-finance-api/src/main/java/com/szmsd/finance/dto/RechargesCallbackRequestDTO.java
+++ business-center/wms-center/wms-service-api/wms-business-finance-api/src/main/java/com/szmsd/finance/dto/RechargesCallbackRequestDTO.java
@@ -14,28 +14,28 @@ import lombok.experimental.Accessors;
 public class RechargesCallbackRequestDTO {
 
     @ApiModelProperty(value = "充值编号")
-    private String rechargeNo;
+    private String RechargeNo;
 
     @ApiModelProperty(value = "充值金额")
-    private RechargesCallbackAmountDTO rechargeAmount;
+    private RechargesCallbackAmountDTO RechargeAmount;
 
     @ApiModelProperty(value = "手续费")
-    private RechargesCallbackAmountDTO transactionFee;
+    private RechargesCallbackAmountDTO TransactionFee;
 
     @ApiModelProperty(value = "实际到账金额")
-    private RechargesCallbackAmountDTO actualRechargeAmount;
+    private RechargesCallbackAmountDTO ActualRechargeAmount;
 
     @ApiModelProperty(value = "充值状态")
-    private String status;
+    private String Status;
 
     @ApiModelProperty(value = "第三方充值地址")
-    private String rechargeUrl;
+    private String RechargeUrl;
 
     @ApiModelProperty(value = "请求参数唯一标识")
-    private String serialNo;
+    private String SerialNo;
 
     @ApiModelProperty(value = "备注")
-    private String remark;
+    private String Remark;
 
     @ApiModelProperty(value = "返回消息")
     private String Message;
