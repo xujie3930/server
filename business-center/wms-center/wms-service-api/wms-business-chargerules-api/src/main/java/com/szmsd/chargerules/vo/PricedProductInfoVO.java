b31da2a7ec753c13c156551da6cd18598bc00cfc
梁超 <1572578128@qq.com>
2021-03-24 13:40:49 +0800

    PRC修改、库存sku体积统计修改

--- business-center/wms-center/wms-service-api/wms-business-chargerules-api/src/main/java/com/szmsd/chargerules/vo/PricedProductInfoVO.java
+++ business-center/wms-center/wms-service-api/wms-business-chargerules-api/src/main/java/com/szmsd/chargerules/vo/PricedProductInfoVO.java
@@ -56,13 +56,25 @@ public class PricedProductInfoVO {
 
     // 有妥投轨迹
 
-    @ApiModelProperty(value = "郡/省/州必填 - 包裹的州必须填写")
+    @ApiModelProperty(value = "州/省必填 - 包裹的州必须填写")
     private Boolean stateRequire;
 
-    @ApiModelProperty(value = "城/镇必填 - 包裹的城市必须填写")
+    @ApiModelProperty(value = "城镇/城市必填   - 包裹的城市必须填写")
     private Boolean cityRequire;
 
-    @ApiModelProperty(value = "郡/省/州验证 - 包裹的州验证")
+    @ApiModelProperty(value = "州/省长度限制1 - 包裹的州最小长度限制")
+    private Integer minStateLength;
+
+    @ApiModelProperty(value = "州/省长度限制2 - 包裹的州长度限制")
+    private Integer stateLength;
+
+    @ApiModelProperty(value = "城镇/城市长度限制1 - 包裹的最小城市长度限制")
+    private Integer minCityLength;
+
+    @ApiModelProperty(value = "城镇/城市长度限制2 - 包裹的城市长度限制")
+    private Integer cityLength;
+
+    @ApiModelProperty(value = "州/省验证 - 包裹的州验证")
     private Boolean stateValifition;
 
     @ApiModelProperty(value = "邮编必填 - 包裹的邮编必须填写")
@@ -71,35 +83,14 @@ public class PricedProductInfoVO {
     @ApiModelProperty(value = "邮编验证 - 包裹的邮编验证")
     private Boolean postCodeValifition;
 
-    @ApiModelProperty(value = "收件人必填 - 包裹的收件人必须填写")
-    private Boolean recipientRequire;
-
-    @ApiModelProperty(value = "Email必填 - 包裹的电子邮件Email必须填写")
-    private Boolean emailRequire;
-
-    @ApiModelProperty(value = "电话必填 - 包裹的电话号码必须填写")
-    private Boolean phoneRequire;
-
-    @ApiModelProperty(value = "电话验证 - 包裹的电话号码验证")
-    private Boolean phoneValifition;
-
-    @ApiModelProperty(value = "郡/省/州长度限制1 - 包裹的州最小长度限制")
-    private Integer minStateLength;
-
-    @ApiModelProperty(value = "郡/省/州长度限制2 - 包裹的州长度限制")
-    private Integer stateLength;
-
-    @ApiModelProperty(value = "城市长度限制1 - 包裹的最小城市长度限制")
-    private Integer minCityLength;
-
-    @ApiModelProperty(value = "城市长度限制2 - 包裹的城市长度限制")
-    private Integer cityLength;
-
     @ApiModelProperty(value = "邮编长度限制1 - 邮编最小长度限制")
     private Integer minPostCodeLength;
 
     @ApiModelProperty(value = "邮编长度限制2 - 邮编长度限制")
     private Integer postCodeLength;
+
+    @ApiModelProperty(value = "收件人必填 - 包裹的收件人必须填写")
+    private Boolean recipientRequire;
 
     @ApiModelProperty(value = "收件人长度限制1 - 包裹的最小收件人长度限制")
     private Integer minRecipientLength;
@@ -107,11 +98,20 @@ public class PricedProductInfoVO {
     @ApiModelProperty(value = "收件人长度限制2 - 包裹的收件人长度限制")
     private Integer recipientLength;
 
+    @ApiModelProperty(value = "Email必填 - 包裹的电子邮件Email必须填写")
+    private Boolean emailRequire;
+
     @ApiModelProperty(value = "Email长度限制1 - 包裹的最小电子邮件Email长度限制")
     private Integer minEmailLength;
 
     @ApiModelProperty(value = "Email长度限制2 - 包裹的电子邮件Email长度限制")
     private Integer emailLength;
+
+    @ApiModelProperty(value = "电话必填 - 包裹的电话号码必须填写")
+    private Boolean phoneRequire;
+
+    @ApiModelProperty(value = "电话验证 - 包裹的电话号码验证")
+    private Boolean phoneValifition;
 
     @ApiModelProperty(value = "电话长度限制1 - 包裹的最小电话号码长度限制")
     private Integer minPhoneLength;
@@ -137,9 +137,16 @@ public class PricedProductInfoVO {
     @ApiModelProperty(value = "地址2长度限制2 - 服务的地址2长度限制（Address2）")
     private Integer addressStreet2Length;
 
-    // 地址总长度限制
+    @ApiModelProperty(value = "地址2长度限制1 - 服务的最小地址总长度限制(Address1+Address2+Address3)")
+    private Integer minAddressLength;
 
-    @ApiModelProperty(value = "挂号服务名称")
+    @ApiModelProperty(value = "地址2长度限制2 - 服务的地址长度限制(Address1+Address2+Address3)")
+    private Integer addressLength;
+
+    @ApiModelProperty(value = "服务商名称")
+    private String terminalCarrier;
+
+    @ApiModelProperty(value = "服务渠道名称 - 挂号服务名称")
     private String logisticsRouteId;
 
     @ApiModelProperty(value = "黑名单")
