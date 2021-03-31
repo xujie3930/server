37e69d568133bfb0e61e6aa7088a5967bd4e6647
zhuzhipeng <820897153@qq.com>
2021-03-31 10:43:49 +0800

    修改模块名 VMS->WMS 提交相关sql

---
+++ business-center/wms-center/wms-service/wms-business-returnex/src/main/java/com/szmsd/returnex/service/IReturnExpressGoodService.java
@@ -0,0 +1,20 @@
+package com.szmsd.returnex.service;
+
+import com.szmsd.returnex.domain.ReturnExpressGood;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
+
+/**
+ * <p>
+ * return_express - 退货单sku详情表 服务类
+ * </p>
+ *
+ * @author 11
+ * @since 2021-03-29
+ */
+public interface IReturnExpressGoodService extends IService<ReturnExpressGood> {
+
+
+}
+
