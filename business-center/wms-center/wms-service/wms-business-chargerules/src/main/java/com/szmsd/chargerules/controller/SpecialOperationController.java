73d8007be1441623279a13d8a33078ce9c571a66
lihao <lihao@szmsd.com>
2021-04-09 09:58:50 +0800

    唯一索引冲突异常处理

--- business-center/wms-center/wms-service/wms-business-chargerules/src/main/java/com/szmsd/chargerules/controller/SpecialOperationController.java
+++ business-center/wms-center/wms-service/wms-business-chargerules/src/main/java/com/szmsd/chargerules/controller/SpecialOperationController.java
@@ -8,6 +8,7 @@ import com.szmsd.common.core.web.controller.BaseController;
 import com.szmsd.common.core.web.page.TableDataInfo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,8 +26,15 @@ public class SpecialOperationController extends BaseController {
     @PreAuthorize("@ss.hasPermi('SpecialOperation:SpecialOperation:add')")
     @ApiOperation(value = "特殊操作计费规则 - 保存")
     @PostMapping("/save")
-    public R save(@RequestBody SpecialOperationDTO dto){
-        return toOk(specialOperationService.save(dto));
+    public R save(@RequestBody SpecialOperationDTO dto) {
+        int save = 0;
+        try {
+            save = specialOperationService.save(dto);
+        } catch (DuplicateKeyException e) {
+            log.error(e.getMessage(), e);
+            return R.failed("操作类型+仓库不能重复");
+        }
+        return toOk(save);
     }
 
     @PreAuthorize("@ss.hasPermi('SpecialOperation:SpecialOperation:edit')")
@@ -39,7 +47,7 @@ public class SpecialOperationController extends BaseController {
     @PreAuthorize("@ss.hasPermi('SpecialOperation:SpecialOperation:list')")
     @ApiOperation(value = "特殊操作计费规则 - 分页查询")
     @GetMapping("/list")
-    public TableDataInfo<SpecialOperation> listPage(SpecialOperationDTO dto){
+    public TableDataInfo<SpecialOperation> listPage(SpecialOperationDTO dto) {
         startPage();
         List<SpecialOperation> list = specialOperationService.listPage(dto);
         return getDataTable(list);
