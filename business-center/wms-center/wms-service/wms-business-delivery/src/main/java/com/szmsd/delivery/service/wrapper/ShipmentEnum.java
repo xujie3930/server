140a8528abbef57bf0a1ecb721c75e9b73068a0a
zhangyuyuan <zhangyuyuan@szmsd.com>
2021-04-12 13:32:13 +0800

    1.修改承运商物流订单编号值。

--- business-center/wms-center/wms-service/wms-business-delivery/src/main/java/com/szmsd/delivery/service/wrapper/ShipmentEnum.java
+++ business-center/wms-center/wms-service/wms-business-delivery/src/main/java/com/szmsd/delivery/service/wrapper/ShipmentEnum.java
@@ -142,7 +142,7 @@ public enum ShipmentEnum implements ApplicationState, ApplicationRegister {
             updateDelOutbound.setExceptionMessage(exceptionMessage);
             // 创建承运商物流订单
             updateDelOutbound.setTrackingNo(delOutbound.getTrackingNo());
-            updateDelOutbound.setOrderNo(delOutbound.getOrderNo());
+            updateDelOutbound.setShipmentOrderNumber(delOutbound.getShipmentOrderNumber());
             // 冻结费用
             updateDelOutbound.setCalcWeight(delOutbound.getCalcWeight());
             updateDelOutbound.setCalcWeightUnit(delOutbound.getCalcWeightUnit());
