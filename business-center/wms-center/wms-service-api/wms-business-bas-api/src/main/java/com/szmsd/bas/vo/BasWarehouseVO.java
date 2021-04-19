6c7e9e261fc6ad42e6e656a2bff1aafe9c0fd8c8
梁超 <1572578128@qq.com>
2021-04-19 17:32:46 +0800

    列表VAT显示修改

--- business-center/wms-center/wms-service-api/wms-business-bas-api/src/main/java/com/szmsd/bas/vo/BasWarehouseVO.java
+++ business-center/wms-center/wms-service-api/wms-business-bas-api/src/main/java/com/szmsd/bas/vo/BasWarehouseVO.java
@@ -39,7 +39,7 @@ public class BasWarehouseVO {
     private String address;
 
     @ApiModelProperty(value = "VAT")
-    @FieldJsonI18n(localLanguageType = LocalLanguageTypeEnum.YN)
+    @FieldJsonI18n(localLanguageType = LocalLanguageTypeEnum.NEED)
     private String isCheckVat;
 
     @ApiModelProperty(value = "状态：0无效，1有效")
