b1c18c32e983e554e25a1c1c81b7ff94335660e2
梁超 <1572578128@qq.com>
2021-04-26 11:03:31 +0800

    入库单审核接口修改 支持批量

--- business-center/wms-center/wms-service-api/wms-business-putinstorage-api/src/main/java/com/szmsd/putinstorage/domain/dto/InboundReceiptReviewDTO.java
+++ business-center/wms-center/wms-service-api/wms-business-putinstorage-api/src/main/java/com/szmsd/putinstorage/domain/dto/InboundReceiptReviewDTO.java
@@ -5,13 +5,15 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+import java.util.List;
+
 @Data
 @Accessors(chain = true)
 @ApiModel(value = "InboundReceiptDTO", description = "入库参数")
 public class InboundReceiptReviewDTO {
 
     @ApiModelProperty(value = "入库单号")
-    private String warehouseNo;
+    private List<String> warehouseNos;
 
     @ApiModelProperty(value = "状态3审核通过，-3审核失败")
     private String status;
