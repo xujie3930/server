9a37bf2a237ecdb6b0b125e66cf7c854205e02b5
lihao <lihao@szmsd.com>
2021-04-30 09:13:52 +0800

    验货

---
+++ business-center/wms-center/wms-service-api/wms-business-inventory-api/src/main/java/com/szmsd/inventory/domain/dto/InventoryInspectionDetailsDTO.java
@@ -0,0 +1,26 @@
+package com.szmsd.inventory.domain.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+
+@Data
+public class InventoryInspectionDetailsDTO {
+
+    @ApiModelProperty(value = "客户代码")
+    private String sku;
+
+    @ApiModelProperty(value = "仓库代码")
+    private String warehouseCode;
+
+    @ApiModelProperty(value = "仓库名称")
+    private String warehouseCodeName;
+
+    @ApiModelProperty(value = "客户代码")
+    private String customCode;
+
+    @ApiModelProperty(value = "客户名称")
+    private String customName;
+
+
+}
