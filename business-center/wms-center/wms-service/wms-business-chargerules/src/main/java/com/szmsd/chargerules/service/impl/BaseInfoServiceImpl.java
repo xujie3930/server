bcab67f03f1111da7f633c731e09823add849cdc
lihao <lihao@szmsd.com>
2021-04-30 10:21:55 +0800

    特殊操作单 显示全部

--- business-center/wms-center/wms-service/wms-business-chargerules/src/main/java/com/szmsd/chargerules/service/impl/BaseInfoServiceImpl.java
+++ business-center/wms-center/wms-service/wms-business-chargerules/src/main/java/com/szmsd/chargerules/service/impl/BaseInfoServiceImpl.java
@@ -119,7 +119,8 @@ public class BaseInfoServiceImpl extends ServiceImpl<BaseInfoMapper, BasSpecialO
         if (StringUtils.isNotEmpty(dto.getOperationType())) {
             where.eq("operation_type", dto.getOperationType());
         }
-        where.eq("status",SpecialOperationStatusEnum.PENDING.getStatus());
+        where.orderByDesc("status");
+//        where.eq("status",SpecialOperationStatusEnum.PENDING.getStatus());
         return baseInfoMapper.selectPageList(where);
     }
 
