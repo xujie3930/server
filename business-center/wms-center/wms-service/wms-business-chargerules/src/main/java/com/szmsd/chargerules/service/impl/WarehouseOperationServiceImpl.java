d6e496565bb60909b5b58f7179ffc853da52c100
lihao <lihao@szmsd.com>
2021-05-13 15:28:09 +0800

    仓库存在直接新增

--- business-center/wms-center/wms-service/wms-business-chargerules/src/main/java/com/szmsd/chargerules/service/impl/WarehouseOperationServiceImpl.java
+++ business-center/wms-center/wms-service/wms-business-chargerules/src/main/java/com/szmsd/chargerules/service/impl/WarehouseOperationServiceImpl.java
@@ -14,6 +14,7 @@ import com.szmsd.common.core.exception.com.AssertUtil;
 import com.szmsd.common.core.exception.com.CommonException;
 import com.szmsd.common.core.utils.bean.BeanMapperUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -41,38 +42,47 @@ public class WarehouseOperationServiceImpl extends ServiceImpl<WarehouseOperatio
     @Transactional
     @Override
     public int save(WarehouseOperationDTO dto) {
-        checkWarehouse(dto);
-        WarehouseOperation domain = BeanMapperUtil.map(dto, WarehouseOperation.class);
-        AssertUtil.notEmpty(dto.getDetails(), "详情必填");
-        warehouseOperationMapper.insert(domain);
-        long count = dto.getDetails().stream().map(value -> value.setWarehouseOperationId(domain.getId())).filter(value -> !Pattern.matches(REGEX, value.getChargeDays())).count();
-        if (count > 0) throw new CommonException("999", "计费天数格式错误");
-        List<WarehouseOperationVo> warehouseOperationDb = this.listPage(dto);
-        if(isIntersection(dto.getDetails(), warehouseOperationDb.get(0).getDetails())) throw new CommonException("999","仓库+区间存在重合");
-        warehouseOperationDetailsService.saveBatch(dto.getDetails());
+        if (!checkWarehouse(dto)) {
+            WarehouseOperation domain = BeanMapperUtil.map(dto, WarehouseOperation.class);
+            AssertUtil.notEmpty(dto.getDetails(), "详情必填");
+            warehouseOperationMapper.insert(domain);
+            long count = dto.getDetails().stream().map(value -> value.setWarehouseOperationId(domain.getId())).filter(value -> !Pattern.matches(REGEX, value.getChargeDays())).count();
+            if (count > 0) throw new CommonException("999", "计费天数格式错误");
+            List<WarehouseOperationVo> warehouseOperationDb = this.listPage(dto);
+            if (isIntersection(dto.getDetails(), warehouseOperationDb.get(0).getDetails()))
+                throw new CommonException("999", "仓库+区间存在重合");
+            warehouseOperationDetailsService.saveBatch(dto.getDetails());
+        }
         return 1;
     }
 
     /**
      * 检验仓库是否存在 ：仓库唯一
+     *
      * @param dto dto
+     * @return true:仓库存在新增 false仓库不存在新增
      */
-    private void checkWarehouse(WarehouseOperationDTO dto) {
+    private boolean checkWarehouse(WarehouseOperationDTO dto) {
         AssertUtil.notNull(dto.getWarehouseCode(), "仓库必填");
-        LambdaQueryWrapper<WarehouseOperation> query = Wrappers.lambdaQuery();
-        query.eq(WarehouseOperation::getWarehouseCode, dto.getWarehouseCode());
-        int count = this.count(query);
-        if(count > 0) throw new CommonException("999","仓库已存在");
+        List<WarehouseOperationVo> list = this.listPage(dto);
+        if (CollectionUtils.isNotEmpty(list)) { //仓库已存在 添加到该仓库下面的详情
+            if (isIntersection(dto.getDetails(), list.get(0).getDetails()))
+                throw new CommonException("999", "仓库+区间存在重合");
+            List<WarehouseOperationDetails> collect = dto.getDetails().stream().map(value -> value.setWarehouseOperationId(list.get(0).getId())).collect(Collectors.toList());
+            return warehouseOperationDetailsService.saveBatch(collect);
+        }
+        return false;
     }
 
     /**
      * 检验天数是否合法
+     *
      * @param dto list
-     * @param db db
+     * @param db  db
      * @return true：重合/不合法 false：不重合/合法
      */
     public boolean isIntersection(List<WarehouseOperationDetails> dto, List<WarehouseOperationDetails> db) {
-        if(dto.size() == 1 && db.size() == 0) return false;
+        if (dto.size() == 1 && db.size() == 0) return false;
         List<WarehouseOperationDetails> list = Stream.of(dto, db).flatMap(Collection::stream).collect(Collectors.toList());
         for (int i = 0; i < list.size(); i++) {
             String[] ds = list.get(i).getChargeDays().split("D");
@@ -83,7 +93,7 @@ public class WarehouseOperationServiceImpl extends ServiceImpl<WarehouseOperatio
                 int start2 = Integer.parseInt(ds2[0]);
                 int end2 = Integer.parseInt(ds2[1]);
                 if (Math.max(start, start2) < Math.min(end, end2)) {
-                    log.error("区间存在重叠交叉！start:{}, end:{}, start2:{}, end2:{}",start,end,start2,end2);
+                    log.error("区间存在重叠交叉！start:{}, end:{}, start2:{}, end2:{}", start, end, start2, end2);
                     return true;
                 }
             }
@@ -95,7 +105,7 @@ public class WarehouseOperationServiceImpl extends ServiceImpl<WarehouseOperatio
     @Transactional
     @Override
     public int update(WarehouseOperationDTO dto) {
-        long count = dto.getDetails().stream().filter(value -> !Pattern.matches(REGEX, value.getChargeDays())).count();
+        long count = dto.getDetails().stream().map(value -> value.setWarehouseOperationId(dto.getId())).filter(value -> !Pattern.matches(REGEX, value.getChargeDays())).count();
         if (count > 0) throw new CommonException("999", "计费天数格式错误");
         WarehouseOperation map = BeanMapperUtil.map(dto, WarehouseOperation.class);
         this.updateDetails(dto);
@@ -106,7 +116,7 @@ public class WarehouseOperationServiceImpl extends ServiceImpl<WarehouseOperatio
         LambdaQueryWrapper<WarehouseOperationDetails> query = Wrappers.lambdaQuery();
         query.eq(WarehouseOperationDetails::getWarehouseOperationId, dto.getId());
         warehouseOperationDetailsService.remove(query);
-        if(isIntersection(dto.getDetails(),new ArrayList<>())) throw new CommonException("999","仓库+区间存在重合");
+        if (isIntersection(dto.getDetails(), new ArrayList<>())) throw new CommonException("999", "仓库+区间存在重合");
         warehouseOperationDetailsService.saveBatch(dto.getDetails());
     }
 
