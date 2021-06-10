0b09bb80f725894e13137cb9538a61d328b8007b
lihao <lihao@szmsd.com>
2021-06-10 09:42:56 +0800

    流水账单导出支持勾选导出

--- business-center/wms-center/wms-service/wms-business-finance/src/main/java/com/szmsd/finance/service/impl/AccountSerialBillServiceImpl.java
+++ business-center/wms-center/wms-service/wms-business-finance/src/main/java/com/szmsd/finance/service/impl/AccountSerialBillServiceImpl.java
@@ -63,6 +63,9 @@ public class AccountSerialBillServiceImpl extends ServiceImpl<AccountSerialBillM
         if (StringUtils.isNotBlank(dto.getCreateTimeEnd())) {
             query.le(AccountSerialBill::getCreateTime, dto.getCreateTimeEnd());
         }
+        if(StringUtils.isNotBlank(dto.getIds())) {
+            query.in(AccountSerialBill::getId, (Object[]) dto.getIds().split(","));
+        }
         return accountSerialBillMapper.selectPageList(query);
     }
 
