36cab41d094cc4792f74137785def39f86eee6aa
梁超 <1572578128@qq.com>
2021-06-11 14:30:16 +0800

    PRC修改产品服务新增子产品字段

--- business-center/wms-center/wms-service-api/wms-business-chargerules-api/src/main/java/com/szmsd/chargerules/dto/UpdateProductDTO.java
+++ business-center/wms-center/wms-service-api/wms-business-chargerules-api/src/main/java/com/szmsd/chargerules/dto/UpdateProductDTO.java
@@ -2,14 +2,20 @@ package com.szmsd.chargerules.dto;
 
 import com.szmsd.chargerules.vo.PricedProductInfoVO;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
+
+import java.util.List;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
 @ApiModel(value = "UpdateProductDTO", description = "修改产品服务")
 public class UpdateProductDTO extends PricedProductInfoVO {
+
+    @ApiModelProperty(value = "子产品")
+    private List<String> subProducts;
 
 }
