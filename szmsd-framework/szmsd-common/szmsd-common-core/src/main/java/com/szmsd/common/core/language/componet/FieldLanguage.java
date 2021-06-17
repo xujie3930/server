eee81b6d704638b7b90264c9d5c87f5b377ef7d5
梁超 <1572578128@qq.com>
2021-06-17 15:00:14 +0800

    入库单审核、上架日志修改 多语言工具类完善

--- szmsd-framework/szmsd-common/szmsd-common-core/src/main/java/com/szmsd/common/core/language/componet/FieldLanguage.java
+++ szmsd-framework/szmsd-common/szmsd-common-core/src/main/java/com/szmsd/common/core/language/componet/FieldLanguage.java
@@ -8,18 +8,13 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.ContextualSerializer;
 import com.szmsd.common.core.language.annotation.FieldJsonI18n;
 import com.szmsd.common.core.language.enums.LanguageEnum;
-import com.szmsd.common.core.language.enums.LocalLanguageEnum;
 import com.szmsd.common.core.language.enums.LocalLanguageTypeEnum;
 import com.szmsd.common.core.language.util.LanguageUtil;
 import com.szmsd.common.core.utils.StringUtils;
-import com.szmsd.common.redis.service.RedisService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.Resource;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * 序列化语言
@@ -30,9 +25,6 @@ import java.util.List;
 @Component
 @Slf4j
 public class FieldLanguage extends JsonSerializer<String> implements ContextualSerializer {
-
-    @Resource
-    private RedisService redisService;
 
     private String jsonI18nType;
 
@@ -47,22 +39,21 @@ public class FieldLanguage extends JsonSerializer<String> implements ContextualS
     public FieldLanguage() {
     }
 
-    public FieldLanguage(String jsonI18nType, LanguageEnum jsonI18nLanguage, String jsonI18nValue, LocalLanguageTypeEnum jsonI18nLocalLanguage, boolean isMultiple, RedisService redisService) {
+    public FieldLanguage(String jsonI18nType, LanguageEnum jsonI18nLanguage, String jsonI18nValue, LocalLanguageTypeEnum jsonI18nLocalLanguage, boolean isMultiple) {
         this.jsonI18nType = jsonI18nType;
         this.jsonI18nLanguage = jsonI18nLanguage;
         this.jsonI18nValue = jsonI18nValue;
         this.jsonI18nLocalLanguage = jsonI18nLocalLanguage;
         this.isMultiple = isMultiple;
-        this.redisService = redisService;
     }
 
     @Override
     public void serialize(String value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
         try {
             if (jsonI18nLocalLanguage != LocalLanguageTypeEnum.SYSTEM_LANGUAGE) {
-                gen.writeString(getLocalLanguage(value, isMultiple));
+                gen.writeString(LanguageUtil.getLanguage(isMultiple, jsonI18nLocalLanguage, value, jsonI18nLanguage));
             } else if (StringUtils.isNotEmpty(jsonI18nType)) {
-                String name = getLanguage(jsonI18nType, value, isMultiple);
+                String name = LanguageUtil.getLanguage(isMultiple, jsonI18nType, value);
                 gen.writeString(name);
             } else {
                 gen.writeString(jsonI18nValue);
@@ -79,52 +70,7 @@ public class FieldLanguage extends JsonSerializer<String> implements ContextualS
         if (annotation == null) {
             return prov.findNullValueSerializer(property);
         }
-        return new FieldLanguage(annotation.type(), annotation.language(), annotation.value(), annotation.localLanguageType(), annotation.isMultiple(), this.redisService);
-    }
-
-    private String getLanguage(String type, String value, boolean isMultiple) {
-        if (!isMultiple) {
-            return LanguageUtil.getLanguage(redisService, type, value);
-        }
-        String[] split = value.split(",");
-        List<String> sb = new ArrayList<>();
-        for (String s : split) {
-            String language = LanguageUtil.getLanguage(redisService, type, s);
-            language = StringUtils.isNotEmpty(language) ? language : s;
-            sb.add(language);
-        }
-        return String.join(",", sb);
-    }
-
-    private String getLocalLanguage(String value, boolean isMultiple) {
-        if (!isMultiple) {
-            return getLocalLanguage(value);
-        }
-        String[] split = value.split(",");
-        List<String> sb = new ArrayList<>();
-        for (String s : split) {
-            String language = getLocalLanguage(s);
-            language = StringUtils.isNotEmpty(language) ? language : s;
-            sb.add(language);
-        }
-        return String.join(",", sb);
-    }
-    private String getLocalLanguage(String value) {
-        LocalLanguageEnum localLanguageEnum = LocalLanguageEnum.getLocalLanguageEnum(jsonI18nLocalLanguage, value);
-        if (localLanguageEnum == null) {
-            log.error("没有维护[{}]枚举语言[{}]", jsonI18nLocalLanguage, value);
-            return value;
-        }
-        String language;
-        switch (LanguageUtil.getLen(jsonI18nLanguage)) {
-            case "enName":
-                language = localLanguageEnum.getEhName();
-                break;
-            default:
-                language = localLanguageEnum.getZhName();
-                break;
-        }
-        return language;
+        return new FieldLanguage(annotation.type(), annotation.language(), annotation.value(), annotation.localLanguageType(), annotation.isMultiple());
     }
 
 }
