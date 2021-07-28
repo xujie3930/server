8f217f869d18d8eeda6195fe26ff3e3fface6214
zhangyuyuan <zhangyuyuan@szmsd.com>
2021-07-28 17:42:11 +0800

    1.修改doc

--- business-center/wms-center/wms-service/wms-business-doc/src/main/java/com/szmsd/doc/BusinessDocApplication.java
+++ business-center/wms-center/wms-service/wms-business-doc/src/main/java/com/szmsd/doc/BusinessDocApplication.java
@@ -11,7 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @EnableSwaggerBootstrapUI
-@EnableFeignClients
+@EnableFeignClients(basePackages = {"com.szmsd"})
 @EnableHystrix
 @EnableDiscoveryClient
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
