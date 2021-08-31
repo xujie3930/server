ed601deca41f15a8caf12db8fd9ae1990a208f20
zhuzhipeng <820897153@qq.com>
2021-08-31 18:37:29 +0800

    fix bug 修改提示错误语句

--- business-center/wms-center/wms-service/wms-business-doc/src/main/java/com/szmsd/doc/api/warehouse/req/BasWarehouseQueryReq.java
+++ business-center/wms-center/wms-service/wms-business-doc/src/main/java/com/szmsd/doc/api/warehouse/req/BasWarehouseQueryReq.java
@@ -12,22 +12,22 @@ import javax.validation.constraints.Size;
 @Accessors(chain = true)
 @ApiModel(value = "BasWarehouseQueryReq", description = "仓库列表查询条件")
 public class BasWarehouseQueryReq extends QueryDto {
-    @Size(max = 30)
+    @Size(max = 30, message = "仓库代码 仅支持 0-30 字符")
     @ApiModelProperty(value = "仓库代码 [30]")
     private String warehouseCode;
-    @Size(max = 200)
+    @Size(max = 200, message = "仓库中文名 仅支持 0-200 字符")
     @ApiModelProperty(value = "仓库中文名 [200]")
     private String warehouseNameCn;
-    @Size(max = 30)
+    @Size(max = 30, message = "国家 仅支持 0-30 字符")
     @ApiModelProperty(value = "国家 - 代码 [30]")
     private String countryCode;
-    @Size(max = 200)
+    @Size(max = 200, message = "省 仅支持 0-200 字符")
     @ApiModelProperty(value = "省 [200]")
     private String province;
-    @Size(max = 200)
+    @Size(max = 200, message = "城市 仅支持 0-200 字符")
     @ApiModelProperty(value = "城市 [200]")
     private String city;
-    @Size(max = 10)
+    @Size(max = 10, message = "状态 仅支持 0-10 字符")
     @ApiModelProperty(value = "状态：0无效，1有效 [10]")
     private String status;
 
