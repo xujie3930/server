3824967fd3fa345574bf0a29c0f50a7bc7b4a9ce
zhangyuyuan <zhangyuyuan@szmsd.com>
2021-09-23 16:14:50 +0800

    1.处理参数不全2的问题。

--- business-center/wms-center/wms-service/wms-business-delivery/src/main/java/com/szmsd/delivery/service/wrapper/impl/DelOutboundAsyncServiceImpl.java
+++ business-center/wms-center/wms-service/wms-business-delivery/src/main/java/com/szmsd/delivery/service/wrapper/impl/DelOutboundAsyncServiceImpl.java
@@ -342,9 +342,9 @@ public class DelOutboundAsyncServiceImpl implements IDelOutboundAsyncService {
      */
     private void deduction(String orderNo, String warehouseCode, String orderType) {
         // 转运出库，集运出库在核重之后会去冻结库存，现在需要进行扣减库存
-        /*if (DelOutboundServiceImplUtil.noOperationInventory(orderType)) {
+        if (DelOutboundServiceImplUtil.noOperationInventory(orderType)) {
             return;
-        }*/
+        }
         // 查询明细
         List<DelOutboundDetail> details = this.delOutboundDetailService.listByOrderNo(orderNo);
         InventoryOperateListDto inventoryOperateListDto = new InventoryOperateListDto();
