fdb50c699c3aa403625225e591a50cee9750a49b
zhuzhipeng <820897153@qq.com>
2021-09-27 14:07:29 +0800

    修改参数校验

--- business-center/wms-center/wms-service/wms-business-doc/src/main/java/com/szmsd/doc/api/delivery/request/DelOutboundBatchPackingDetailRequest.java
+++ business-center/wms-center/wms-service/wms-business-doc/src/main/java/com/szmsd/doc/api/delivery/request/DelOutboundBatchPackingDetailRequest.java
@@ -4,15 +4,20 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 @Data
 @ApiModel(description = "DelOutboundBatchPackingRequest对象")
 public class DelOutboundBatchPackingDetailRequest implements Serializable {
-
+    @NotBlank(message = "sku不能为空")
     @ApiModelProperty(value = "SKU")
     private String sku;
 
+    @NotNull(message = "数量不能为空")
+    @Min(value = 0,message = "数量异常")
     @ApiModelProperty(value = "数量")
     private Long qty;
 
--- business-center/wms-center/wms-service/wms-business-doc/src/main/java/com/szmsd/doc/api/delivery/request/DelOutboundBatchPackingRequest.java
+++ business-center/wms-center/wms-service/wms-business-doc/src/main/java/com/szmsd/doc/api/delivery/request/DelOutboundBatchPackingRequest.java
@@ -5,35 +5,41 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.List;
 
 @Data
 @ApiModel(description = "包装信息 - 选则按要求装箱需要填写")
 public class DelOutboundBatchPackingRequest implements Serializable {
-
+    @NotNull(message = "数量不能为空")
+    @Min(value = 0,message = "数量异常")
     @ApiModelProperty(value = "数量")
     private Long qty;
 
-    @ApiModelProperty(value = "箱号")
+    @ApiModelProperty(value = "箱号",hidden = true)
     private String packingNo;
 
-    @ApiModelProperty(value = "长 CM")
+    @ApiModelProperty(value = "长 CM",hidden = true)
     private Double length;
 
-    @ApiModelProperty(value = "宽 CM")
+    @ApiModelProperty(value = "宽 CM",hidden = true)
     private Double width;
 
-    @ApiModelProperty(value = "高 CM")
+    @ApiModelProperty(value = "高 CM",hidden = true)
     private Double height;
 
-    @ApiModelProperty(value = "重量 g")
+    @ApiModelProperty(value = "重量 g",hidden = true)
     private Double weight;
 
-    @ApiModelProperty(value = "包材类型")
+    @ApiModelProperty(value = "包材类型",hidden = true)
     private String packingMaterial;
 
     @Valid
+    @NotEmpty(message = "明细不能为空")
     @ApiModelProperty(value = "明细")
     private List<DelOutboundBatchPackingDetailRequest> details;
 }
