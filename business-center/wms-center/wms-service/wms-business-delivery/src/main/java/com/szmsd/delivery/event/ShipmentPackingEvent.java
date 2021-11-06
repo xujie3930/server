5a8832a5db466237fc394058099aff8737f82a5f
zhangyuyuan <zhangyuyuan@szmsd.com>
2021-11-06 14:24:51 +0800

    1.增加日志。

--- business-center/wms-center/wms-service/wms-business-delivery/src/main/java/com/szmsd/delivery/event/ShipmentPackingEvent.java
+++ business-center/wms-center/wms-service/wms-business-delivery/src/main/java/com/szmsd/delivery/event/ShipmentPackingEvent.java
@@ -1,5 +1,7 @@
 package com.szmsd.delivery.event;
 
+import com.szmsd.delivery.config.AsyncThreadObject;
+import org.slf4j.MDC;
 import org.springframework.context.ApplicationEvent;
 
 /**
@@ -10,7 +12,14 @@ import org.springframework.context.ApplicationEvent;
  */
 public class ShipmentPackingEvent extends ApplicationEvent {
 
+    private final AsyncThreadObject asyncThreadObject;
+
     public ShipmentPackingEvent(Object source) {
         super(source);
+        this.asyncThreadObject = new AsyncThreadObject();
+    }
+
+    public AsyncThreadObject getAsyncThreadObject() {
+        return asyncThreadObject;
     }
 }
--- business-center/wms-center/wms-service/wms-business-delivery/src/main/java/com/szmsd/delivery/event/listener/ShipmentPackingListener.java
+++ business-center/wms-center/wms-service/wms-business-delivery/src/main/java/com/szmsd/delivery/event/listener/ShipmentPackingListener.java
@@ -1,9 +1,12 @@
 package com.szmsd.delivery.event.listener;
 
+import com.alibaba.fastjson.JSON;
+import com.szmsd.delivery.config.AsyncThreadObject;
 import com.szmsd.delivery.config.ThreadPoolExecutorConfiguration;
 import com.szmsd.delivery.event.ShipmentPackingEvent;
-import com.szmsd.delivery.service.IDelOutboundPackageQueueService;
 import com.szmsd.delivery.service.wrapper.IDelOutboundAsyncService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
@@ -15,21 +18,31 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class ShipmentPackingListener {
+    private final Logger logger = LoggerFactory.getLogger(ShipmentPackingListener.class);
 
     @Autowired
     private IDelOutboundAsyncService delOutboundAsyncService;
-    @Autowired
-    private IDelOutboundPackageQueueService delOutboundPackageQueueService;
 
     @Async(value = ThreadPoolExecutorConfiguration.THREADPOOLEXECUTOR_SHIPMENTPACKINGEVENT)
     @EventListener
     public void onApplicationEvent(ShipmentPackingEvent event) {
-        if (null != event.getSource()) {
-            this.delOutboundAsyncService.shipmentPacking((Long) event.getSource());
-//            DelOutboundPackageQueue queue = new DelOutboundPackageQueue();
-//            queue.setOrderId((Long) event.getSource());
-//            queue.setNextHandleTime(new Date());
-//            this.delOutboundPackageQueueService.insertDelOutboundPackageQueue(queue);
+        Thread thread = Thread.currentThread();
+        AsyncThreadObject asyncThreadObject = event.getAsyncThreadObject();
+        boolean isAsyncThread = !asyncThreadObject.isAsyncThread();
+        logger.info("(1)任务开始执行，当前任务名称：{}，当前任务ID：{}，是否为异步任务：{}，任务相关参数：{}", thread.getName(), thread.getId(), isAsyncThread, JSON.toJSONString(event));
+        if (isAsyncThread) {
+            // 是异步的线程，将主线程的tid赋值到当前线程的TID上
+            asyncThreadObject.loadTid();
+        }
+        try {
+            if (null != event.getSource()) {
+                this.delOutboundAsyncService.shipmentPacking((Long) event.getSource());
+            }
+        } finally {
+            if (isAsyncThread) {
+                // 异步线程执行完成之后将值清空
+                asyncThreadObject.unloadTid();
+            }
         }
     }
 }
