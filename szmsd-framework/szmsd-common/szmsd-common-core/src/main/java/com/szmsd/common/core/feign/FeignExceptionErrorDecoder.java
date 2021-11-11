dcd7013b84ea7967a6befa769e5996e5a17ee1e8
zhangyuyuan <zhangyuyuan@szmsd.com>
2021-11-11 19:02:26 +0800

    1.feign接口调用decode逻辑调整。

--- szmsd-framework/szmsd-common/szmsd-common-core/src/main/java/com/szmsd/common/core/feign/FeignExceptionErrorDecoder.java
+++ szmsd-framework/szmsd-common/szmsd-common-core/src/main/java/com/szmsd/common/core/feign/FeignExceptionErrorDecoder.java
@@ -4,7 +4,6 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.szmsd.common.core.domain.R;
 import com.szmsd.common.core.exception.com.CommonException;
-import com.szmsd.common.core.exception.com.SystemException;
 import feign.Response;
 import feign.Util;
 import feign.codec.ErrorDecoder;
@@ -12,7 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 
 import java.nio.charset.StandardCharsets;
 
@@ -29,20 +27,18 @@ public class FeignExceptionErrorDecoder implements ErrorDecoder {
                 log.info("feign decode body {}", JSONObject.toJSONString(body));
                 R<?> r = JSON.parseObject(body, R.class);
                 if (null != r) {
-                    if (HttpStatus.BAD_REQUEST.value() == r.getCode()) {
-                        throw new CommonException(String.valueOf(r.getCode()), r.getMsg());
-                    } else {
-                        throw new SystemException(String.valueOf(r.getCode()), r.getMsg());
-                    }
+                    throw new CommonException(String.valueOf(r.getCode()), r.getMsg());
                 }
+                // 返回无法解析的错误信息
+                throw new CommonException("500", body);
             }
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
             if (e instanceof CommonException) {
                 return e;
             }
-            return new SystemException("500", e.getMessage());
+            return new CommonException("500", e.getMessage());
         }
-        return new SystemException("500", "系统异常,请联系管理员");
+        return new CommonException("500", "系统异常,请联系管理员");
     }
 }
