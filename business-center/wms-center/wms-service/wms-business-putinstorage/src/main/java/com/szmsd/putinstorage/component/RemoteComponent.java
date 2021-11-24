469c2c1b195adad6566d3a2e1ed098d99adb7456
zhuzhipeng <820897153@qq.com>
2021-11-24 11:22:00 +0800

    x

--- business-center/wms-center/wms-service/wms-business-putinstorage/src/main/java/com/szmsd/putinstorage/component/RemoteComponent.java
+++ business-center/wms-center/wms-service/wms-business-putinstorage/src/main/java/com/szmsd/putinstorage/component/RemoteComponent.java
@@ -332,7 +332,7 @@ public class RemoteComponent {
         if (CollectionUtils.isEmpty(createInboundReceiptDTO.getInboundReceiptDetails())) return;
         CreateTrackRequest createTrackRequest = new CreateTrackRequest();
         createTrackRequest.setWarehouseCode(createInboundReceiptDTO.getWarehouseCode())
-                .setRefOrderNo(createInboundReceiptDTO.getOrderNo())
+                .setRefOrderNo(createInboundReceiptDTO.getWarehouseNo())
                 .setTrackingNumberList(createInboundReceiptDTO.getDeliveryNoList());
         log.info("创建入库单物流信息列表 {}", createTrackRequest);
         R<ResponseVO> tracking = htpInboundFeignService.createTracking(createTrackRequest);
@@ -344,7 +344,7 @@ public class RemoteComponent {
         // 创建入库单物流信息列表
         CreateTrackRequest createTrackRequest = new CreateTrackRequest();
         createTrackRequest.setWarehouseCode(inboundReceipt.getWarehouseCode())
-                .setRefOrderNo(inboundReceipt.getOrderNo())
+                .setRefOrderNo(inboundReceipt.getWarehouseNo())
                 .setTrackingNumberList(updateTrackingNoRequest.getDeliveryNoList());
         log.info("更新入库单物流信息列表 {}", createTrackRequest);
         R<ResponseVO> tracking = htpInboundFeignService.createTracking(createTrackRequest);
