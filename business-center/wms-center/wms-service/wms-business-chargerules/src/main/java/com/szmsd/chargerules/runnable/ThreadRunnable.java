35f7a31e87c51da88ba4ce12ce003a5f061d8f4b
zhuzhipeng <820897153@qq.com>
2021-12-03 09:38:58 +0800

    x

--- business-center/wms-center/wms-service/wms-business-chargerules/src/main/java/com/szmsd/chargerules/runnable/ThreadRunnable.java
+++ business-center/wms-center/wms-service/wms-business-chargerules/src/main/java/com/szmsd/chargerules/runnable/ThreadRunnable.java
@@ -65,7 +65,7 @@ public class ThreadRunnable {
      * 定时任务：储存仓租计价扣费；每周日晚上8点执行
      */
 //    @Scheduled(cron = "0/60 * * * * *")
-    @Scheduled(cron = "0 0 20 ? * 1 *")
+    @Scheduled(cron = "0 0 20 ? * 1")
     public void executeWarehouse() {
         log.info("executeWarehouse() start...");
         RLock lock = redissonClient.getLock("executeOperation");
