c29e464370abcd4081533e54e0e8f12bec2186ed
zhuzhipeng <820897153@qq.com>
2022-01-14 14:50:14 +0800

    CHANGE: 账单修改列表展示的金额展示问题

--- business-center/wms-center/wms-service/wms-business-finance/src/main/java/com/szmsd/finance/service/impl/AccountSerialBillServiceImpl.java
+++ business-center/wms-center/wms-service/wms-business-finance/src/main/java/com/szmsd/finance/service/impl/AccountSerialBillServiceImpl.java
@@ -1,21 +1,33 @@
 package com.szmsd.finance.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.szmsd.common.core.constant.HttpStatus;
 import com.szmsd.common.core.utils.bean.BeanMapperUtil;
+import com.szmsd.common.core.web.page.TableDataInfo;
 import com.szmsd.common.datascope.annotation.DataScope;
+import com.szmsd.delivery.api.feign.DelOutboundFeignService;
+import com.szmsd.delivery.dto.DelOutboundListQueryDto;
+import com.szmsd.delivery.vo.DelOutboundListVO;
 import com.szmsd.finance.domain.AccountSerialBill;
 import com.szmsd.finance.dto.AccountSerialBillDTO;
 import com.szmsd.finance.mapper.AccountSerialBillMapper;
 import com.szmsd.finance.service.IAccountSerialBillService;
 import com.szmsd.finance.service.ISysDictDataService;
+import com.szmsd.putinstorage.api.feign.InboundReceiptFeignService;
+import com.szmsd.putinstorage.domain.dto.InboundReceiptQueryDTO;
+import com.szmsd.putinstorage.domain.vo.InboundReceiptVO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -65,10 +77,122 @@ public class AccountSerialBillServiceImpl extends ServiceImpl<AccountSerialBillM
         if (StringUtils.isNotBlank(dto.getCreateTimeEnd())) {
             query.le(AccountSerialBill::getCreateTime, dto.getCreateTimeEnd());
         }
-        if(StringUtils.isNotBlank(dto.getIds())) {
+        if (StringUtils.isNotBlank(dto.getIds())) {
             query.in(AccountSerialBill::getId, (Object[]) dto.getIds().split(","));
         }
-        return accountSerialBillMapper.selectPageList(query);
+        List<AccountSerialBill> accountSerialBills = accountSerialBillMapper.selectPageList(query);
+        // 修改下单时间等信息
+        showProcess(accountSerialBills);
+        return accountSerialBills;
+    }
+
+    @Resource
+    private InboundReceiptFeignService inboundReceiptFeignService;
+    @Resource
+    private DelOutboundFeignService delOutboundFeignService;
+
+    @Data
+    @AllArgsConstructor
+    private static class ListProcess {
+        private Date orderTime;
+        private String no;
+        private String warehouseCode;
+
+    }
+
+    /**
+     * 账单展示仓库 下单时间
+     *
+     * @param accountSerialBills
+     */
+    public void showProcess(List<AccountSerialBill> accountSerialBills) {
+        ArrayList<AccountSerialBill> resultList = new ArrayList<>();
+        List<AccountSerialBill> noList = accountSerialBills.stream().filter(x -> StringUtils.isNotBlank(x.getNo())).collect(Collectors.toList());
+
+        CompletableFuture<List<ListProcess>> listCompletableFuture = CompletableFuture.supplyAsync(() -> {
+            log.info("ruku--------------------------");
+            List<AccountSerialBill> rk = noList.parallelStream().filter(x -> x.getNo().startsWith("RK")).distinct().collect(Collectors.toList());
+            String rkNo = rk.stream().map(AccountSerialBill::getNo).distinct().collect(Collectors.joining(","));
+
+            List<ListProcess> result = new ArrayList<>();
+            if (StringUtils.isNotBlank(rkNo)) {
+                InboundReceiptQueryDTO inboundReceiptQueryDTO = new InboundReceiptQueryDTO();
+                inboundReceiptQueryDTO.setWarehouseNo(rkNo);
+                TableDataInfo<InboundReceiptVO> inboundReceiptVOTableDataInfo = inboundReceiptFeignService.postPage(inboundReceiptQueryDTO);
+                if (inboundReceiptVOTableDataInfo.getCode() == HttpStatus.SUCCESS) {
+                    List<InboundReceiptVO> ckPage = inboundReceiptVOTableDataInfo.getRows();
+                    Map<String, InboundReceiptVO> collect = ckPage.stream().collect(Collectors.toMap(InboundReceiptVO::getWarehouseNo, x -> x, (x1, x2) -> x1));
+                    result = rk.stream().map(x -> {
+                        InboundReceiptVO inboundReceiptVO = collect.get(x.getNo());
+                        if (Objects.nonNull(inboundReceiptVO)) {
+                            Date createTime = inboundReceiptVO.getCreateTime();
+                            String warehouseCode = inboundReceiptVO.getWarehouseCode();
+                            return new ListProcess(createTime, x.getNo(), warehouseCode);
+                        }
+                        return null;
+                    }).filter(Objects::nonNull).collect(Collectors.toList());
+                }
+            }
+            log.info("ruku--------------------------");
+            return result;
+        });
+
+
+        CompletableFuture<List<ListProcess>> listCompletableFuture1 = CompletableFuture.supplyAsync(() -> {
+            log.info("CK--------------------------");
+            List<AccountSerialBill> ck = noList.parallelStream().filter(x -> x.getNo().startsWith("CK")).distinct().collect(Collectors.toList());
+            String ckNo = ck.stream().map(AccountSerialBill::getNo).distinct().collect(Collectors.joining(","));
+            if (StringUtils.isNotBlank(ckNo)) {
+                DelOutboundListQueryDto delOutboundListQueryDto = new DelOutboundListQueryDto();
+                delOutboundListQueryDto.setOrderNo(ckNo);
+                TableDataInfo<DelOutboundListVO> page = delOutboundFeignService.page(delOutboundListQueryDto);
+                if (page.getCode() == HttpStatus.SUCCESS) {
+                    List<DelOutboundListVO> rows = page.getRows();
+                    Map<String, DelOutboundListVO> collect = rows.stream().collect(Collectors.toMap(DelOutboundListVO::getOrderNo, x -> x, (x1, x2) -> x1));
+                    log.info("CK--------------------------");
+                    return ck.stream().map(x -> {
+                        DelOutboundListVO delOutboundListVO = collect.get(x.getNo());
+                        if (Objects.nonNull(delOutboundListVO)) {
+                            Date createTime = delOutboundListVO.getCreateTime();
+                            String warehouseCode = delOutboundListVO.getWarehouseCode();
+                            x.setOrderTime(createTime);
+                            x.setWarehouseCode(warehouseCode);
+                            return new ListProcess(createTime, x.getNo(), warehouseCode);
+                        }
+                        return null;
+                    }).filter(Objects::nonNull).collect(Collectors.toList());
+                }
+            }
+            return new ArrayList<>();
+        });
+
+        Void join = CompletableFuture.allOf(listCompletableFuture1, listCompletableFuture).join();
+
+        List<ListProcess> listProcesses = listCompletableFuture.join();
+        List<ListProcess> listProcesses1 = listCompletableFuture1.join();
+        ArrayList<ListProcess> queryResult = new ArrayList<>();
+        queryResult.addAll(listProcesses);
+        queryResult.addAll(listProcesses1);
+        Map<String, ListProcess> queryResultMap = queryResult.stream().collect(Collectors.toMap(ListProcess::getNo, x -> x, (x1, x2) -> x1));
+        log.info("===========================================");
+        log.info(JSONObject.toJSONString(queryResultMap));
+        log.info("===========================================");
+        List<String> positiveNumber = Arrays.asList("线下充值","退费","优惠");
+        long count = accountSerialBills.parallelStream().peek(x -> {
+            if (StringUtils.isNotBlank(x.getNo())) {
+                Optional.ofNullable(queryResultMap.get(x.getNo())).ifPresent(queryResultNo -> {
+                    x.setWarehouseCode(queryResultNo.getWarehouseCode());
+                    x.setOrderTime(queryResultNo.getOrderTime());
+                });
+            }
+            if (!(StringUtils.isNotBlank(x.getChargeType()) && positiveNumber.contains(x.getChargeType()))) {
+                // 设置
+                Optional.ofNullable(x.getAmount()).ifPresent(amount -> {
+                    x.setAmount(amount.negate());
+                });
+            }
+        }).count();
+
     }
 
     @Override
