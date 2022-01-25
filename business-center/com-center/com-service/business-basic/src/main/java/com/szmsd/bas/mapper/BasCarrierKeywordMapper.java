8c6008519f2921585a05c94b50b5b41957bec801
zengfanlang <zengfanlang@szmsd.com>
2022-01-25 10:13:18 +0800

    fix: 新增关键词维护

---
+++ business-center/com-center/com-service/business-basic/src/main/java/com/szmsd/bas/mapper/BasCarrierKeywordMapper.java
@@ -0,0 +1,16 @@
+package com.szmsd.bas.mapper;
+
+import com.szmsd.bas.domain.BasCarrierKeyword;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author YM
+ * @since 2022-01-24
+ */
+public interface BasCarrierKeywordMapper extends BaseMapper<BasCarrierKeyword> {
+
+}
