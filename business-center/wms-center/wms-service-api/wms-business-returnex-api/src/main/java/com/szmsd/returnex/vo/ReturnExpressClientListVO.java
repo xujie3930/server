a61aad06307a73a5cb16bbbc204d277da3121b97
zhuzhipeng <820897153@qq.com>
2022-02-19 09:30:23 +0800

    导出

---
+++ business-center/wms-center/wms-service-api/wms-business-returnex-api/src/main/java/com/szmsd/returnex/vo/ReturnExpressClientListVO.java
@@ -0,0 +1,74 @@
+package com.szmsd.returnex.vo;
+
+import com.alibaba.fastjson.JSONObject;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.szmsd.bas.plugin.BasSubCodeCommonParameter;
+import com.szmsd.bas.plugin.BasSubCommonPlugin;
+import com.szmsd.common.core.annotation.Excel;
+import com.szmsd.common.core.language.annotation.FieldJsonI18n;
+import com.szmsd.common.core.language.constant.RedisLanguageTable;
+import com.szmsd.common.core.language.enums.LocalLanguageTypeEnum;
+import com.szmsd.common.plugin.annotation.AutoFieldValue;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+/**
+ * @ClassName: ReturnExpressVO
+ * @Description: 退款单列表对象
+ * @Author: 11
+ * @Date: 2021/3/26 13:41
+ */
+@Data
+@Accessors(chain = true)
+@ApiModel(description = "退货单列表详情VO")
+public class ReturnExpressClientListVO implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Excel(name = "预报单号")
+    @ApiModelProperty(value = "预报单号")
+    private String expectedNo;
+    @Excel(name = "WMS处理单号")
+    @ApiModelProperty(value = "WMS处理单号 退件单号")
+    private String returnNo;
+    @Excel(name = "类型")
+    @FieldJsonI18n(type = RedisLanguageTable.BAS_SUB)
+    @ApiModelProperty(value = "退件单来源[默认：1：退件预报2：VMS通知退件]", hidden = true)
+    private String returnSourceStr;
+    @Excel(name = "退件目标仓库")
+    @FieldJsonI18n(type = RedisLanguageTable.BAS_WAREHOUSE)
+    @ApiModelProperty(value = "目的仓库名称", hidden = true)
+    private String warehouseCodeStr;
+    @Excel(name = "退件类型")
+    @FieldJsonI18n(type = RedisLanguageTable.BAS_SUB)
+    @ApiModelProperty(value = "退件类型", hidden = true)
+    private String returnTypeStr;
+    @Excel(name = "申请处理方式")
+    @ApiModelProperty(value = "申请处理方式", hidden = true)
+    private String processTypeStr;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Excel(name = "到仓时间", dateFormat = "yyyy-MM-dd HH:mm:ss")
+    @ApiModelProperty(value = "到仓时间")
+    private Date arrivalTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Excel(name = "完成时间", dateFormat = "yyyy-MM-dd HH:mm:ss")
+    @ApiModelProperty(value = "完成时间")
+    private LocalDateTime finishTime;
+    @Excel(name = "是否逾期")
+    @FieldJsonI18n(localLanguageType = LocalLanguageTypeEnum.YN)
+    @ApiModelProperty(value = "是否逾期", hidden = true)
+    private String overdueStr;
+    @Excel(name = "状态")
+    @FieldJsonI18n(localLanguageType = LocalLanguageTypeEnum.RETURN_EXPRESS)
+    @ApiModelProperty(value = "处理状态编码 订单状态", hidden = true)
+    private String dealStatusStr;
+
+    @Override
+    public String toString() {
+        return JSONObject.toJSONString(this);
+    }
+}
--- business-center/wms-center/wms-service/wms-business-returnex/src/main/java/com/szmsd/returnex/controller/ReturnExpressClientController.java
+++ business-center/wms-center/wms-service/wms-business-returnex/src/main/java/com/szmsd/returnex/controller/ReturnExpressClientController.java
@@ -12,11 +12,13 @@ import com.szmsd.returnex.dto.ReturnExpressAddDTO;
 import com.szmsd.returnex.dto.ReturnExpressListQueryDTO;
 import com.szmsd.returnex.dto.ReturnExpressServiceAddDTO;
 import com.szmsd.returnex.service.IReturnExpressService;
+import com.szmsd.returnex.vo.ReturnExpressClientListVO;
 import com.szmsd.returnex.vo.ReturnExpressListVO;
 import com.szmsd.returnex.vo.ReturnExpressVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.apache.commons.io.IOUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
@@ -32,6 +34,7 @@ import java.net.URLEncoder;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @ClassName: ReturnExpressClientController
@@ -102,12 +105,18 @@ public class ReturnExpressClientController extends BaseController {
     @Log(title = "退货服务模块", businessType = BusinessType.UPDATE)
     @ApiOperation(value = "导出")
     public void export(HttpServletResponse httpServerResponse, @Validated ReturnExpressListQueryDTO queryDto) {
-        List<ReturnExpressListVO> list = returnExpressService.selectReturnOrderList(queryDto);
+        List<ReturnExpressListVO> list = returnExpressService.selectClientReturnOrderList(queryDto);
         HandlerContext<List<ReturnExpressListVO>> objectHandlerContext = new HandlerContext<>(list);
         objectHandlerContext.handlerValue();
-        ExcelUtil<ReturnExpressListVO> util = new ExcelUtil<>(ReturnExpressListVO.class);
-        util.exportExcel(httpServerResponse, list, "退货记录-" + LocalDate.now());
+        List<ReturnExpressClientListVO> exportList = list.stream().map(x -> {
+            ReturnExpressClientListVO returnExpressClientListVO = new ReturnExpressClientListVO();
+            BeanUtils.copyProperties(x, returnExpressClientListVO);
+            return returnExpressClientListVO;
+        }).collect(Collectors.toList());
+        ExcelUtil<ReturnExpressClientListVO> util = new ExcelUtil<>(ReturnExpressClientListVO.class);
+        util.exportExcel(httpServerResponse, exportList, "退货记录-" + LocalDate.now());
     }
+
     /**
      * 更新退件单信息
      *
@@ -161,6 +170,7 @@ public class ReturnExpressClientController extends BaseController {
             e.printStackTrace();
         }
     }
+
     /**
      * 获取退件单信息详情
      *
