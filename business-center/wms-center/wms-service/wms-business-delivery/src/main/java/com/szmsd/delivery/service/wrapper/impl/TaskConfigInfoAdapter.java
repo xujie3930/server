028207fe9a8cb69f1f791619e2379e33bbfe32e6
zhangyuyuan <zhangyuyuan@szmsd.com>
2022-04-22 09:48:25 +0800

    fix: 发货计划条件增加自定义处理。

--- business-center/wms-center/wms-service/wms-business-delivery/src/main/java/com/szmsd/delivery/service/wrapper/impl/TaskConfigInfoAdapter.java
+++ business-center/wms-center/wms-service/wms-business-delivery/src/main/java/com/szmsd/delivery/service/wrapper/impl/TaskConfigInfoAdapter.java
@@ -6,11 +6,11 @@ import com.szmsd.http.dto.TaskConfigInfo;
 public class TaskConfigInfoAdapter {
 
     /**
-     * 自提：不接收发货指令、波次打印（需要把客户上传的PDF在一开始创建WMS订单的时候推送）、包材回传：否、重量回传：否
+     * 自提：不接收发货指令、不打印、包材回传：否、重量回传：否
      * <p>
-     * 组合SKU：不接收发货指令、波次打印（需要把客户上传的PDF在一开始创建WMS订单的时候推送）、包材回传：是、重量回传：否
+     * 组合SKU：不接收发货指令、不打印、包材回传：是、重量回传：否
      * <p>
-     * 拆分SKU：不接收发货指令、波次打印（需要把客户上传的PDF在一开始创建WMS订单的时候推送）、包材回传：是、重量回传：否
+     * 拆分SKU：不接收发货指令、不打印、包材回传：是、重量回传：否
      * <p>
      * 销毁的：不接收发货指令、不打印、包材回传：否、重量回传：否
      *
@@ -19,11 +19,11 @@ public class TaskConfigInfoAdapter {
      */
     public static TaskConfigInfo getTaskConfigInfo(String orderType) {
         if (DelOutboundOrderTypeEnum.SELF_PICK.getCode().equals(orderType)) {
-            return buildTaskConfigInfo("NotReceive", false, false, "PrintOnWave");
+            return buildTaskConfigInfo("NotReceive", false, false, "NotPrint");
         } else if (DelOutboundOrderTypeEnum.NEW_SKU.getCode().equals(orderType)) {
-            return buildTaskConfigInfo("NotReceive", true, false, "PrintOnWave");
+            return buildTaskConfigInfo("NotReceive", true, false, "NotPrint");
         } else if (DelOutboundOrderTypeEnum.SPLIT_SKU.getCode().equals(orderType)) {
-            return buildTaskConfigInfo("NotReceive", true, false, "PrintOnWave");
+            return buildTaskConfigInfo("NotReceive", true, false, "NotPrint");
         } else if (DelOutboundOrderTypeEnum.DESTROY.getCode().equals(orderType)) {
             return buildTaskConfigInfo("NotReceive", false, false, "NotPrint");
         }
