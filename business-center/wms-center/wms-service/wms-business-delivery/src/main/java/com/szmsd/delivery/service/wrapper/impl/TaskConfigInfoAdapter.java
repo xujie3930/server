a7b4e68d0242d191050a6d21985011100e70c41e
zhangyuyuan <zhangyuyuan@szmsd.com>
2022-04-22 09:50:16 +0800

    fix: 发货计划条件增加自定义处理。

--- business-center/wms-center/wms-service/wms-business-delivery/src/main/java/com/szmsd/delivery/service/wrapper/impl/TaskConfigInfoAdapter.java
+++ business-center/wms-center/wms-service/wms-business-delivery/src/main/java/com/szmsd/delivery/service/wrapper/impl/TaskConfigInfoAdapter.java
@@ -6,7 +6,7 @@ import com.szmsd.http.dto.TaskConfigInfo;
 public class TaskConfigInfoAdapter {
 
     /**
-     * 自提：不接收发货指令、不打印、包材回传：否、重量回传：否
+     * 自提：不接收发货指令、波次打印、包材回传：否、重量回传：否
      * <p>
      * 组合SKU：不接收发货指令、不打印、包材回传：是、重量回传：否
      * <p>
@@ -19,7 +19,7 @@ public class TaskConfigInfoAdapter {
      */
     public static TaskConfigInfo getTaskConfigInfo(String orderType) {
         if (DelOutboundOrderTypeEnum.SELF_PICK.getCode().equals(orderType)) {
-            return buildTaskConfigInfo("NotReceive", false, false, "NotPrint");
+            return buildTaskConfigInfo("NotReceive", false, false, "PrintOnWave");
         } else if (DelOutboundOrderTypeEnum.NEW_SKU.getCode().equals(orderType)) {
             return buildTaskConfigInfo("NotReceive", true, false, "NotPrint");
         } else if (DelOutboundOrderTypeEnum.SPLIT_SKU.getCode().equals(orderType)) {
