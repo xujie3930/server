321100e60e86764de4d6d9f4ffce9c7d2879f8b2
zhangyuyuan <zhangyuyuan@szmsd.com>
2022-05-19 09:27:50 +0800

    add: 增加查询shopify认证接口。

--- business-center/wms-center/wms-service/wms-business-bas/src/main/java/com/szmsd/bas/controller/BasSellerShopifyPermissionController.java
+++ business-center/wms-center/wms-service/wms-business-bas/src/main/java/com/szmsd/bas/controller/BasSellerShopifyPermissionController.java
@@ -1,12 +1,17 @@
 package com.szmsd.bas.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.szmsd.bas.domain.BasSellerShopifyPermission;
 import com.szmsd.bas.service.IBasSellerShopifyPermissionService;
 import com.szmsd.common.core.domain.R;
 import com.szmsd.common.core.web.controller.BaseController;
+import com.szmsd.common.security.domain.LoginUser;
+import com.szmsd.common.security.utils.SecurityUtils;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -33,12 +38,32 @@ public class BasSellerShopifyPermissionController extends BaseController {
     private IBasSellerShopifyPermissionService basSellerShopifyPermissionService;
 
     @PostMapping(value = "/list")
-    public R<List<BasSellerShopifyPermission>> list(@RequestBody BasSellerShopifyPermission basSellerShopifyPermission) {
+    @ApiImplicitParam(name = "entity", value = "参数", dataType = "BasSellerShopifyPermission")
+    public R<List<BasSellerShopifyPermission>> list(@RequestBody BasSellerShopifyPermission entity) {
         LambdaQueryWrapper<BasSellerShopifyPermission> lambdaQueryWrapper = Wrappers.lambdaQuery();
-        lambdaQueryWrapper.eq(StringUtils.isNotEmpty(basSellerShopifyPermission.getSellerCode()), BasSellerShopifyPermission::getSellerCode, basSellerShopifyPermission.getSellerCode());
-        lambdaQueryWrapper.eq(StringUtils.isNotEmpty(basSellerShopifyPermission.getShop()), BasSellerShopifyPermission::getShop, basSellerShopifyPermission.getShop());
+        lambdaQueryWrapper.eq(StringUtils.isNotEmpty(entity.getSellerCode()), BasSellerShopifyPermission::getSellerCode, entity.getSellerCode());
+        lambdaQueryWrapper.eq(StringUtils.isNotEmpty(entity.getShop()), BasSellerShopifyPermission::getShop, entity.getShop());
         // 查询有效的
         lambdaQueryWrapper.eq(BasSellerShopifyPermission::getState, "1");
         return R.ok(this.basSellerShopifyPermissionService.list(lambdaQueryWrapper));
+    }
+
+    @PostMapping(value = "/page")
+    @ApiImplicitParam(name = "entity", value = "参数", dataType = "BasSellerShopifyPermission")
+    public R<IPage<BasSellerShopifyPermission>> page(@RequestBody BasSellerShopifyPermission entity) {
+        IPage<BasSellerShopifyPermission> iPage = new Page<>(entity.getPageNum(), entity.getPageSize());
+        LambdaQueryWrapper<BasSellerShopifyPermission> lambdaQueryWrapper = Wrappers.lambdaQuery();
+        String sellerCode = entity.getSellerCode();
+        if (StringUtils.isEmpty(sellerCode)) {
+            LoginUser loginUser = SecurityUtils.getLoginUser();
+            if (null != loginUser) {
+                sellerCode = loginUser.getSellerCode();
+            }
+        }
+        if (StringUtils.isEmpty(sellerCode)) {
+            return R.ok(iPage);
+        }
+        lambdaQueryWrapper.eq(BasSellerShopifyPermission::getSellerCode, sellerCode);
+        return R.ok(this.basSellerShopifyPermissionService.page(iPage, lambdaQueryWrapper));
     }
 }
