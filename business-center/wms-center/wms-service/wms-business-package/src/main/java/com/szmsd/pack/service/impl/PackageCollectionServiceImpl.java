f167582a0d29c45126f62e673eb6035e8f4dce24
taojie <taojie@szmsd.com>
2022-08-05 15:35:33 +0800

    admin登录 sellerCode 为空导致查询有问题

--- business-center/wms-center/wms-service/wms-business-package/src/main/java/com/szmsd/pack/service/impl/PackageCollectionServiceImpl.java
+++ business-center/wms-center/wms-service/wms-business-package/src/main/java/com/szmsd/pack/service/impl/PackageCollectionServiceImpl.java
@@ -208,8 +208,10 @@ public class PackageCollectionServiceImpl extends ServiceImpl<PackageCollectionM
                     packageCollection.setCalcWeight(calcWeight.getValue());
                     packageCollection.setCalcWeightUnit(calcWeight.getUnit());
 
-                    if (data.getIsPickupPackageService() != null) packageCollection.setIsPickupPackageService(data.getIsPickupPackageService());
-                    if (data.getPickupPackageServiceName() != null) packageCollection.setPickupPackageServiceName(data.getPickupPackageServiceName());
+                    if (data.getIsPickupPackageService() != null)
+                        packageCollection.setIsPickupPackageService(data.getIsPickupPackageService());
+                    if (data.getPickupPackageServiceName() != null)
+                        packageCollection.setPickupPackageServiceName(data.getPickupPackageServiceName());
 
                     List<ChargeItem> charges = chargeWrapper.getCharges();
                     // 汇总费用
@@ -468,7 +470,7 @@ public class PackageCollectionServiceImpl extends ServiceImpl<PackageCollectionM
         createPickupPackageCommand.setPickupPieces(pickupPieces);
 
         PickupDateInfo pickupDateInfo = new PickupDateInfo();
-        if(packageCollection.getCollectionDate()!=null){
+        if (packageCollection.getCollectionDate() != null) {
             SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
             pickupDateInfo.setPickupDate(sdf.format(packageCollection.getCollectionDate()));
         }
@@ -859,11 +861,13 @@ public class PackageCollectionServiceImpl extends ServiceImpl<PackageCollectionM
             queryWrapper.eq(StringUtils.isNotEmpty(sellerCode), PackageCollection::getSellerCode, sellerCode);
         }
         // 子母单的查询 如果没有传值就只能才自己的
-//        String cusCode = CollectionUtils.isNotEmpty(SecurityUtils.getLoginUser().getPermissions()) ? SecurityUtils.getLoginUser().getPermissions().get(0) : "";
-//        if(StringUtils.isEmpty(dto.getCustomCode())){
-//            dto.setCustomCode(cusCode);
-//        }
-//        queryWrapper.in(PackageCollection::getSellerCode, dto.getCustomCodeList());
+        String cusCode = CollectionUtils.isNotEmpty(SecurityUtils.getLoginUser().getPermissions()) ? SecurityUtils.getLoginUser().getPermissions().get(0) : "";
+        if (StringUtils.isEmpty(dto.getCustomCode())) {
+            dto.setCustomCode(cusCode);
+        }
+        if (CollectionUtils.isNotEmpty(dto.getCustomCodeList())) {
+            queryWrapper.in(PackageCollection::getSellerCode, dto.getCustomCodeList());
+        }
 
         // 揽收单号
         this.autoSettingListCondition(queryWrapper, PackageCollection::getCollectionNo, this.getTextList(dto.getCollectionNo()));
