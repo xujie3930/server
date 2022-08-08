f6a37b657aa38df3ac25fcae79c638e5c3f3827b
taojie <taojie@szmsd.com>
2022-08-08 20:56:00 +0800

    token为null导致SecurityUtils.getLoginUser()为null

--- business-center/wms-center/wms-service/wms-business-putinstorage/src/main/java/com/szmsd/putinstorage/service/impl/InboundReceiptServiceImpl.java
+++ business-center/wms-center/wms-service/wms-business-putinstorage/src/main/java/com/szmsd/putinstorage/service/impl/InboundReceiptServiceImpl.java
@@ -149,9 +149,11 @@ public class InboundReceiptServiceImpl extends ServiceImpl<InboundReceiptMapper,
             List<String> orderNoList = ListUtils.emptyIfNull(queryDTO.getOrderNoList());
             queryDTO.setOrderNoList(Stream.of(orderNoSplit, orderNoList).flatMap(Collection::stream).distinct().collect(Collectors.toList()));
         }
-        String cusCode = CollectionUtils.isNotEmpty(SecurityUtils.getLoginUser().getPermissions()) ? SecurityUtils.getLoginUser().getPermissions().get(0) : "";
-        if(StringUtils.isEmpty(queryDTO.getCusCode())){
-            queryDTO.setCusCode(cusCode);
+        if (Objects.nonNull(SecurityUtils.getLoginUser())) {
+            String cusCode = CollectionUtils.isNotEmpty(SecurityUtils.getLoginUser().getPermissions()) ? SecurityUtils.getLoginUser().getPermissions().get(0) : "";
+            if (StringUtils.isEmpty(queryDTO.getCusCode())) {
+                queryDTO.setCusCode(cusCode);
+            }
         }
         return baseMapper.selectListByCondiction(queryDTO);
     }
