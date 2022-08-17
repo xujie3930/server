deee78932fa56fc0e341e271254e4b4b581eaa0c
jiangjun <jiangjun@szmsd.com>
2022-08-17 19:14:43 +0800

    修改回调命名

--- business-center/wms-center/wms-service/wms-business-http/src/main/java/com/szmsd/http/controller/RedirectUriController.java
+++ business-center/wms-center/wms-service/wms-business-http/src/main/java/com/szmsd/http/controller/RedirectUriController.java
@@ -41,7 +41,7 @@ public class RedirectUriController {
             @ApiImplicitParam(name = "code", value = "授权校验码", required = true)
     })
     public R<String> redirectUri(@RequestParam(value = "state") String state,
-                                 @RequestParam(value = "code") String code) {
+                                 @RequestParam(value = "client_id") String code) {
         String wrapRedirectUriKey = RedirectUriUtil.wrapRedirectUriKey(state);
         logger.info("测试回调是否进入：{}");
         // 设置到缓存中，有效期1小时
