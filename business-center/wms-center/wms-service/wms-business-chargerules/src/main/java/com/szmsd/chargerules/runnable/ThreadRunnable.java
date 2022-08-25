888227ef6fb280b3cf06f16c622520a2e53ff912
jiangjun <jiangjun@szmsd.com>
2022-08-25 14:22:43 +0800

    还原三秒

--- business-center/wms-center/wms-service/wms-business-chargerules/src/main/java/com/szmsd/chargerules/runnable/ThreadRunnable.java
+++ business-center/wms-center/wms-service/wms-business-chargerules/src/main/java/com/szmsd/chargerules/runnable/ThreadRunnable.java
@@ -65,8 +65,8 @@ public class ThreadRunnable {
     /**
      * 定时任务：储存仓租计价扣费；每周日晚上8点执行
      */
-//    @Scheduled(cron = "0/60 * * * * *")  0 0 20 ? * 1
-    @Scheduled(cron = "0/3 * * * * ?")
+//    @Scheduled(cron = "0/60 * * * * *")
+    @Scheduled(cron = "0 0 20 ? * 1")
     public void executeWarehouse() {
         log.info("executeWarehouse() start...");
         RLock lock = redissonClient.getLock("executeOperation");
@@ -120,7 +120,8 @@ public class ThreadRunnable {
             warehouseOperationDTO.setEffectiveTime(new Date());
             warehouseOperationDTO.setExpirationTime(new Date());
             warehouseOperationDTO.setCusCodeList(skuVolume.getCusCode());
-            List<WarehouseOperationVo> warehouseOperationConfig = warehouseOperationService.selectOperationByRule(warehouseOperationDTO);
+            List<WarehouseOperationVo> warehouseOperationConfig = warehouseOperationService
+                    .selectOperationByRule(warehouseOperationDTO);
               /*  List<WarehouseOperationVo> warehouseOperationConfig = warehouseOperationMapper.listPage(new WarehouseOperationDTO().setWarehouseCode(warehouseCode)
                         .setEffectiveTime(now).setExpirationTime(now).setCusCodeList(skuVolume.getCusCode())
                 );*/
