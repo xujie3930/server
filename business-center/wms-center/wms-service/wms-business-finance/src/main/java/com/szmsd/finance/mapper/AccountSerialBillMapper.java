0dd855327a24d8a37b577b15d8281f787e9894f9
weixiaofeng <260008747@qq.com>
2022-09-23 20:10:36 +0800

    【新增】资金结余 / 【新增】资金结余-Server

--- business-center/wms-center/wms-service/wms-business-finance/src/main/java/com/szmsd/finance/mapper/AccountSerialBillMapper.java
+++ business-center/wms-center/wms-service/wms-business-finance/src/main/java/com/szmsd/finance/mapper/AccountSerialBillMapper.java
@@ -3,11 +3,7 @@ package com.szmsd.finance.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.szmsd.delivery.domain.DelOutbound;
 import com.szmsd.finance.domain.AccountSerialBill;
-import com.szmsd.finance.dto.AccountBalanceBillResultDTO;
 import com.szmsd.finance.dto.AccountSerialBillDTO;
-import com.szmsd.finance.vo.BillBusinessTotalVO;
-import com.szmsd.finance.vo.BillDirectDeliveryTotalVO;
-import com.szmsd.finance.vo.EleBillQueryVO;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -20,24 +16,5 @@ public interface AccountSerialBillMapper extends BaseMapper<AccountSerialBill> {
     //根据单号查询其他信息
     DelOutbound  selectDelOutbound(@Param("no") String no);
 
-    List<AccountBalanceBillResultDTO> findAccountBillResultData(EleBillQueryVO eleBillQueryVO);
 
-
-    List<BillBusinessTotalVO> selectBusinessTotal(EleBillQueryVO queryVO);
-
-    List<BillDirectDeliveryTotalVO> selectDirectDelivery(EleBillQueryVO queryVO);
-
-    List<AccountSerialBill> selectBillDetails(EleBillQueryVO billDetailQueryVO);
-
-    List<BillBusinessTotalVO> selectAllOrderType(EleBillQueryVO queryVO);
-
-    List<BillBusinessTotalVO> recharge(EleBillQueryVO queryVO);
-
-    List<BillBusinessTotalVO> withdrawal(EleBillQueryVO queryVO);
-
-    List<BillBusinessTotalVO> supplementary(EleBillQueryVO queryVO);
-
-    List<BillBusinessTotalVO> businessAll(EleBillQueryVO queryVO);
-
-    List<BillBusinessTotalVO> balanceConversion(EleBillQueryVO queryVO);
 }
