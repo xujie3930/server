8a388b717f1acf2ffaaaa8a2c9945de9f1d411b0
weixiaofeng <260008747@qq.com>
2022-09-29 10:59:19 +0800

    储存仓租计价扣费 log

--- business-center/wms-center/wms-service/wms-business-chargerules/src/main/java/com/szmsd/chargerules/runnable/ThreadRunnable.java
+++ business-center/wms-center/wms-service/wms-business-chargerules/src/main/java/com/szmsd/chargerules/runnable/ThreadRunnable.java
@@ -32,6 +32,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.*;
 import java.util.concurrent.Executor;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -66,13 +67,18 @@ public class ThreadRunnable {
 //    @Scheduled(cron = "0/60 * * * * *")
     // @Scheduled(cron = "0 10 0/1 * * ?") 每小时执行一次
     //每周日0 0 20 ? * 1"
-    @Scheduled(cron = "0 0 10 * * ?")
+    @Scheduled(cron = "0 10 11 * * ?")
     public void executeWarehouse() {
         log.info("executeWarehouse() start...");
         RLock lock = redissonClient.getLock("executeOperation");
 
         try {
-            if (lock.tryLock()) {
+
+            boolean ty = lock.tryLock(3, TimeUnit.SECONDS);
+
+            log.info("executeWarehouse() tryLock...{}",ty);
+
+            if (ty) {
 
                 log.info("executeWarehouse() getWarehouseSku...s");
 
@@ -91,7 +97,7 @@ public class ThreadRunnable {
         } catch (Exception e) {
             log.error("executeWarehouse() execute error: ", e);
         } finally {
-            if (lock.isLocked() && lock.isHeldByCurrentThread()){
+            if (lock.isLocked()){
                 lock.unlock();
             }
         }
