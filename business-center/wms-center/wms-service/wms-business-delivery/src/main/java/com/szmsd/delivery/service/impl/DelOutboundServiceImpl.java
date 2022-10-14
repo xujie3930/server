2bd9807432cb5dcddc7da0be94b91cf7c7fa81b5
jiangjun <jiangjun@szmsd.com>
2022-10-14 14:41:01 +0800

    解决邮箱发送 原挂号和新挂号一样的问题

--- business-center/wms-center/wms-service/wms-business-delivery/src/main/java/com/szmsd/delivery/service/impl/DelOutboundServiceImpl.java
+++ business-center/wms-center/wms-service/wms-business-delivery/src/main/java/com/szmsd/delivery/service/impl/DelOutboundServiceImpl.java
@@ -1461,7 +1461,6 @@ public class DelOutboundServiceImpl extends ServiceImpl<DelOutboundMapper, DelOu
                 delOutboundTarckOn.setTrackingNo(delOutbound.getTrackingNo());
                 delOutboundTarckOn.setUpdateTime(new Date());
                 delOutboundTarckOn.setTrackingNoNew(updateTrackingNoDto.getTrackingNo());
-                int u = super.baseMapper.updateTrackingNo(updateTrackingNoDto);
                 delOutboundTarckOnMapper.insertSelective(delOutboundTarckOn);
                 ShipmentTrackingChangeRequestDto shipmentTrackingChangeRequestDto=new ShipmentTrackingChangeRequestDto();
                 shipmentTrackingChangeRequestDto.setTrackingNo(updateTrackingNoDto.getTrackingNo());
@@ -1620,6 +1619,10 @@ public class DelOutboundServiceImpl extends ServiceImpl<DelOutboundMapper, DelOu
 
             }
         }
+        list.forEach(x->{
+            int u = super.baseMapper.updateTrackingNo(x);
+
+        });
 
     }
 
