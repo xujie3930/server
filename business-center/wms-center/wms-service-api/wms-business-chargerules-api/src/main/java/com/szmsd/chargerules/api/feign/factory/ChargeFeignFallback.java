be69b4796896cf662ab0cecae407c7905608fba8
zhouyonglai <zhouyonglai@szmsd.com>
2022-10-18 19:16:05 +0800

    类型转换错误修复

--- business-center/wms-center/wms-service-api/wms-business-chargerules-api/src/main/java/com/szmsd/chargerules/api/feign/factory/ChargeFeignFallback.java
+++ business-center/wms-center/wms-service-api/wms-business-chargerules-api/src/main/java/com/szmsd/chargerules/api/feign/factory/ChargeFeignFallback.java
@@ -29,7 +29,7 @@ public class ChargeFeignFallback implements FallbackFactory<ChargeFeignService> 
             }
 
             @Override
-            public R selectBasProductService(List<String> list) {
+            public R<List<BasProductService>> selectBasProductService(List<String> list) {
                 return R.convertResultJson(throwable);
             }
 
