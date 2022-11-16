7cbcd3e15202ac788b20e339b1f7972193f55efa
weixiaofeng <260008747@qq.com>
2022-11-16 14:45:57 +0800

    8688 业务明细的导出以及汇总导出，有数据不全。

--- business-center/wms-center/wms-service-api/wms-business-finance-api/src/main/java/com/szmsd/finance/dto/AccountSerialBillDTO.java
+++ business-center/wms-center/wms-service-api/wms-business-finance-api/src/main/java/com/szmsd/finance/dto/AccountSerialBillDTO.java
@@ -1,23 +1,27 @@
 package com.szmsd.finance.dto;
 
-import com.szmsd.common.core.annotation.Excel;
 import com.szmsd.common.core.utils.StringToolkit;
 import com.szmsd.common.core.utils.StringUtils;
-import com.szmsd.common.plugin.annotation.AutoFieldI18n;
 import com.szmsd.finance.enums.BillEnum;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class AccountSerialBillDTO {
+public class AccountSerialBillDTO implements Serializable {
+
+    private static final long serialVersionUID = 13242786321243523L;
 
     @ApiModelProperty(value = "当前起始页索引")
     private int pageNum;
@@ -197,5 +201,10 @@ public class AccountSerialBillDTO {
         this.orderTime = details.getOrderTime();
         this.paymentTime = details.getPaymentTime();
         this.remark = details.getRemark();
+        this.shipmentRuleName = details.getShipmentRuleName();
+        this.shipmentRule = details.getShipmentRule();
+        this.amazonLogisticsRouteId = details.getAmazonLogisticsRouteId();
+        this.country = details.getCountry();
+        this.countryCode = details.getCountryCode();
     }
 }
