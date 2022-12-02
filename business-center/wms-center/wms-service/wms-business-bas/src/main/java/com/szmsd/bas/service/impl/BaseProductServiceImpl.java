d43897ed4c667b08862ab9a2473c4f9db145062b
jiangjun <jiangjun@szmsd.com>
2022-12-02 15:04:04 +0800

    登录修改验证码和增加sku查询打印日志

--- business-center/wms-center/wms-service/wms-business-bas/src/main/java/com/szmsd/bas/service/impl/BaseProductServiceImpl.java
+++ business-center/wms-center/wms-service/wms-business-bas/src/main/java/com/szmsd/bas/service/impl/BaseProductServiceImpl.java
@@ -148,6 +148,8 @@ public class BaseProductServiceImpl extends ServiceImpl<BaseProductMapper, BaseP
                 queryDto.setSellerCodes(cusCode);
             }
         }
+        log.info("doc查询sku绑定的客户code：{}", queryDto.getCodes());
+        log.info("doc查询sku绑定的客户codes：{}", queryDto.getSellerCodes());
         if (StringUtils.isNotEmpty(queryDto.getSellerCodes())) {
             String[] sellerCodes = queryDto.getSellerCodes().split(",");
             queryWrapper.in("seller_code", sellerCodes);
