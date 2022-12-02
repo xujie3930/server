54ee2867bb4ce26bf58f20ed58bde1847a9db868
jiangjun <jiangjun@szmsd.com>
2022-12-02 15:30:33 +0800

    sku查询打印登录日志

--- business-center/wms-center/wms-service/wms-business-bas/src/main/java/com/szmsd/bas/service/impl/BaseProductServiceImpl.java
+++ business-center/wms-center/wms-service/wms-business-bas/src/main/java/com/szmsd/bas/service/impl/BaseProductServiceImpl.java
@@ -148,6 +148,9 @@ public class BaseProductServiceImpl extends ServiceImpl<BaseProductMapper, BaseP
                 queryDto.setSellerCodes(cusCode);
             }
         }
+
+        log.info("doc查询sku打印token信息：{}",  SecurityUtils.getLoginUser());
+
         log.info("doc查询sku绑定的客户code：{}", queryDto.getCodes());
         log.info("doc查询sku绑定的客户codes：{}", queryDto.getSellerCodes());
         if (StringUtils.isNotEmpty(queryDto.getSellerCodes())) {
