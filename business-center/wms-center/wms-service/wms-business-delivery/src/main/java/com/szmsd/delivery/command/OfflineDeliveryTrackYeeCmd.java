7d3940fa8d81c99fc9d0862d4fd0280e64b2a1f4
weixiaofeng <260008747@qq.com>
2022-12-05 13:34:56 +0800

    12555 子 L126【调整】包裹查询-新增线下发货导入功能 / 包裹查询-S  推送ty

--- business-center/wms-center/wms-service/wms-business-delivery/src/main/java/com/szmsd/delivery/command/OfflineDeliveryTrackYeeCmd.java
+++ business-center/wms-center/wms-service/wms-business-delivery/src/main/java/com/szmsd/delivery/command/OfflineDeliveryTrackYeeCmd.java
@@ -1,9 +1,15 @@
 package com.szmsd.delivery.command;
 
+import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.szmsd.common.core.command.BasicCommand;
+import com.szmsd.common.core.utils.SpringUtils;
+import com.szmsd.delivery.domain.DelOutbound;
 import com.szmsd.delivery.domain.OfflineDeliveryImport;
 import com.szmsd.delivery.dto.OfflineResultDto;
+import com.szmsd.delivery.service.IDelOutboundService;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,8 +27,25 @@ public class OfflineDeliveryTrackYeeCmd extends BasicCommand<Integer> {
         List<OfflineDeliveryImport> offlineDeliveryImports = offlineResultDto.getOfflineDeliveryImports();
         List<String> trackNos = offlineDeliveryImports.stream().map(OfflineDeliveryImport::getTrackingNo).collect(Collectors.toList());
 
-        
+        IDelOutboundService iDelOutboundService = SpringUtils.getBean(IDelOutboundService.class);
 
-        return null;
+        List<DelOutbound> delOutboundList = iDelOutboundService.list(Wrappers.<DelOutbound>query().lambda().in(DelOutbound::getTrackingNo,trackNos));
+
+        if(CollectionUtils.isEmpty(delOutboundList)){
+            return 0;
+        }
+
+        List<String> ids = new ArrayList<>();
+        for(DelOutbound delOutbound : delOutboundList){
+            Long id = delOutbound.getId();
+            ids.add(id.toString());
+        }
+
+        try {
+            iDelOutboundService.manualTrackingYee(ids);
+        }catch (Exception e){
+            return 0;
+        }
+        return 1;
     }
 }
