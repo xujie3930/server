82a84aa38ccac3c921933b72a9061f6da8d4abe8
weixiaofeng <260008747@qq.com>
2022-12-20 11:28:47 +0800

    Context

--- szmsd-framework/szmsd-common/szmsd-common-core/src/main/java/com/szmsd/common/core/command/BasicCommandConfiguration.java
+++ szmsd-framework/szmsd-common/szmsd-common-core/src/main/java/com/szmsd/common/core/command/BasicCommandConfiguration.java
@@ -1,18 +1,13 @@
 package com.szmsd.common.core.command;
 
-import com.szmsd.common.core.support.Context;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
+//@Configuration
 public class BasicCommandConfiguration {
-
-    @Bean
-    public Context context(ApplicationContext applicationContext) {
-        Context.applicationContext = applicationContext;
-        return new Context();
-    }
-
-
+//
+//    @Bean
+//    public Context context(ApplicationContext applicationContext) {
+//        Context.applicationContext = applicationContext;
+//        return new Context();
+//    }
+//
+//
 }
