8d8ab4939b388042187f9c644b0032f5a473ff3e
weixiaofeng <260008747@qq.com>
2023-01-05 16:44:41 +0800

    9329 对订单多次进行计价，会导致系统对这笔单统计错误

--- business-center/wms-center/wms-service/wms-business-delivery/src/main/java/com/szmsd/delivery/timer/ChargeSecondTimer.java
+++ business-center/wms-center/wms-service/wms-business-delivery/src/main/java/com/szmsd/delivery/timer/ChargeSecondTimer.java
@@ -1,6 +1,8 @@
 package com.szmsd.delivery.timer;
 
 import com.szmsd.delivery.service.ChargeService;
+import com.szmsd.delivery.util.LockerUtil;
+import org.redisson.api.RedissonClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,17 +18,27 @@ public class ChargeSecondTimer {
     @Autowired
     private ChargeService chargeService;
 
+    @Autowired
+    private RedissonClient redissonClient;
+
     /**
      * 二次计费定时器
      * <p/>
      * 每1分执行一次
      */
     @Async
-    @Scheduled(cron = "0 */1 * * * ?")
-    public void autoOfflineDelivery() {
+    @Scheduled(cron = "0 */2 * * * ?")
+    public void doSecondCharge() {
         logger.info("ChargeSecondTimer  二次计费开始========");
-        chargeService.doSecondCharge();
+        String key = "ChargeSecondTimer:doSecondCharge";
+        this.doWorker(key, () -> {
+            chargeService.doSecondCharge();
+        });
         logger.info("ChargeSecondTimer  二次计费结束========");
+    }
+
+    private void doWorker(String key, LockerUtil.Worker worker) {
+        new LockerUtil<Integer>(redissonClient).tryLock(key, worker);
     }
 
 }
