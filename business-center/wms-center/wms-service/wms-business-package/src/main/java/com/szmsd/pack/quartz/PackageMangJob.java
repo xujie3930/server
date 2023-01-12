035ec10019175a5ae8a84c8f94e4c6f1d7e40c7a
jiangjun <jiangjun@szmsd.com>
2023-01-12 13:16:23 +0800

    因为部署环境把周几换成英文，需要手动转换

--- business-center/wms-center/wms-service/wms-business-package/src/main/java/com/szmsd/pack/quartz/PackageMangJob.java
+++ business-center/wms-center/wms-service/wms-business-package/src/main/java/com/szmsd/pack/quartz/PackageMangJob.java
@@ -16,6 +16,7 @@ import org.springframework.scheduling.quartz.QuartzJobBean;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -32,16 +33,15 @@ public class PackageMangJob extends QuartzJobBean {
       List<PackageManagementConfig> packageManagementConfigList=packageManagementConfigMapper.selectPackageManagementConfigJob();
       if (packageManagementConfigList.size()>0){
           packageManagementConfigList.forEach(x->{
-              log.info("自动建揽收单数据：{}",x);
               //当前日期求出周期
-              Date date=new Date();
-              SimpleDateFormat dateFm = new SimpleDateFormat("EEEE");
+//              Date date=new Date();
+//              SimpleDateFormat dateFm = new SimpleDateFormat("EEEE");
 
-              String currSun = dateFm.format(date);
+              String currSun = weekDay();
               log.info("自动建揽收单数据周期：{}",currSun);
               //如果相同生成该周期的揽收单
              if (x.getWeekName().equals(currSun)){
-                 log.info("进来11111111111111111");
+
                  PackageManagement packageManagement=new PackageManagement();
                  BeanUtils.copyProperties(x,packageManagement);
                  packageManagement.setId(null);
@@ -49,10 +49,23 @@ public class PackageMangJob extends QuartzJobBean {
                  packageManagement.setCreateTime(new Date());
                  packageManagement.setCreateByName("系统自动创建");
                  iPackageMangServeService.insertPackageManagementjob(packageManagement);
-                 log.info("自动建揽收单数据成功");
              }
           });
       }
+
+    }
+
+    public String weekDay() {
+        Date date = new Date();
+        String[] weekDays = {"星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"};
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+
+        int i = calendar.get(Calendar.DAY_OF_WEEK) - 1;
+        if (i < 0) {
+            i = 0;
+        }
+      return weekDays[i];
     }
 
 }
