ccf4e0586b7e9af02ae63818de73c11bd691b742
weixiaofeng <260008747@qq.com>
2023-02-03 17:32:49 +0800

    9634 一票多件，一票多件出库单推给wms ordertype 字段的值应该用的是：MultiBox

--- business-center/wms-center/wms-service-api/wms-business-chargerules-api/src/main/java/com/szmsd/chargerules/enums/DelOutboundOrderEnum.java
+++ business-center/wms-center/wms-service-api/wms-business-chargerules-api/src/main/java/com/szmsd/chargerules/enums/DelOutboundOrderEnum.java
@@ -19,7 +19,7 @@ public enum DelOutboundOrderEnum {
     BATCH_LABEL("Batch-label", "批量出库单-贴标费"),
     FREEZE_IN_STORAGE("FreezeInStorage", "入库冻结"),
     PackageCollection("PackageCollection","揽收单"),
-    MULTIPLE_PIECES("MultiplePieces","一票多件出库单"),
+    MULTIBOX("MultiBox","一票多件出库单"),
     BULK_ORDER("BulkOrder","大货订单"),
 
     ;
