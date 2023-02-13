156ff088d5122524f700931f3fbb7edb9c34d888
weixiaofeng <260008747@qq.com>
2023-02-13 10:40:59 +0800

    13838 子 【调整】TY轨迹-TY相关功能迁移新模块 / TY轨迹-S

--- business-center/wms-center/wms-service-api/wms-business-track-api/src/main/java/com/szmsd/track/api/feign/TrackFeignService.java
+++ business-center/wms-center/wms-service-api/wms-business-track-api/src/main/java/com/szmsd/track/api/feign/TrackFeignService.java
@@ -1,6 +1,6 @@
 package com.szmsd.track.api.feign;
 
-import com.szmsd.delivery.api.BusinessDeliveryInterface;
+import com.szmsd.track.api.BusinessTrackInterface;
 import com.szmsd.track.api.feign.factory.TrackFeignFallback;
 import com.szmsd.track.domain.DelTrack;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
  * @author wxf
  * @date
  */
-@FeignClient(contextId = "FeignClient.TrackFeignService", name = BusinessDeliveryInterface.SERVICE_NAME, fallbackFactory = TrackFeignFallback.class)
+@FeignClient(contextId = "FeignClient.TrackFeignService", name = BusinessTrackInterface.SERVICE_NAME, fallbackFactory = TrackFeignFallback.class)
 public interface TrackFeignService {
 
     @PostMapping("/api/track/addData")
