9c02691ec238dec3a4d157cddff69623633811df
weixiaofeng <260008747@qq.com>
2023-02-14 16:10:49 +0800

    13819 【新增】TY轨迹-TY轨迹关键词条映射

--- business-center/wms-center/wms-service/wms-business-track/src/main/java/com/szmsd/track/controller/TrackConfigruleController.java
+++ business-center/wms-center/wms-service/wms-business-track/src/main/java/com/szmsd/track/controller/TrackConfigruleController.java
@@ -1,10 +1,13 @@
 package com.szmsd.track.controller;
 
+import com.szmsd.common.core.domain.R;
 import com.szmsd.common.core.web.controller.BaseController;
 import com.szmsd.common.core.web.page.TableDataInfo;
 import com.szmsd.track.domain.TrackConfigRule;
 import com.szmsd.track.service.TrackConfigruleService;
 import com.szmsd.track.vo.TrackConfigRuleQueryVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +24,7 @@ import java.util.List;
  * @author wxf
  * @since 2023-02-14
  */
+@Api(tags = {"轨迹配置"})
 @Controller
 @RequestMapping("/track-config-rule")
 public class TrackConfigruleController extends BaseController {
@@ -28,6 +32,7 @@ public class TrackConfigruleController extends BaseController {
     @Autowired
     private TrackConfigruleService configruleService;
 
+    @ApiOperation(value = "分页查询", notes = "分页查询")
     @GetMapping(value = "/")
     public TableDataInfo<TrackConfigRule> list(TrackConfigRuleQueryVO trackConfigRuleQueryVO) {
 
@@ -36,23 +41,27 @@ public class TrackConfigruleController extends BaseController {
         return getDataTable(trackConfigRules);
     }
 
+    @ApiOperation(value = "根据ID获取详情", notes = "根据ID获取详情")
     @GetMapping(value = "/{id}")
-    public ResponseEntity<TrackConfigRule> getById(@PathVariable("id") String id) {
-        return new ResponseEntity<>(configruleService.getById(id), HttpStatus.OK);
+    public R<TrackConfigRule> getById(@PathVariable("id") String id) {
+        return R.ok(configruleService.getById(id));
     }
 
+    @ApiOperation(value = "保存配置", notes = "保存配置")
     @PostMapping(value = "/create")
-    public ResponseEntity<Object> create(@RequestBody TrackConfigRule params) {
+    public R create(@RequestBody TrackConfigRule params) {
         configruleService.save(params);
-        return new ResponseEntity<>("created successfully", HttpStatus.OK);
+        return R.ok();
     }
 
+    @ApiOperation(value = "删除配置", notes = "删除配置")
     @PostMapping(value = "/delete/{id}")
-    public ResponseEntity<Object> delete(@PathVariable("id") String id) {
+    public R delete(@PathVariable("id") String id) {
         configruleService.removeById(id);
-        return new ResponseEntity<>("deleted successfully", HttpStatus.OK);
+        return R.ok();
     }
 
+    @ApiOperation(value = "更新配置", notes = "更新配置")
     @PostMapping(value = "/update")
     public ResponseEntity<Object> delete(@RequestBody TrackConfigRule params) {
         configruleService.updateById(params);
